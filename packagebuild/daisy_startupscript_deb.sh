98413d82d9ca81c8f0b6b5ff477a5ad01e12951e
Liam Hopkins <liamh@google.com>
2019-11-04 23:38:52 -0800

    New package build tooling (#14)
    * common packagebuild workflow
    
    * build scripts
    
    * build only for requested distros
    
    * bugfixes
    
    * update daisy-build container
    
    * use includeworkflows

---
+++ packagebuild/daisy_startupscript_deb.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+# Copyright 2019 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+URL="http://metadata/computeMetadata/v1/instance/attributes"
+GCS_PATH=$(curl -f -H Metadata-Flavor:Google ${URL}/daisy-outs-path)
+SRC_PATH=$(curl -f -H Metadata-Flavor:Google ${URL}/daisy-sources-path)
+REPO_OWNER=$(curl -f -H Metadata-Flavor:Google ${URL}/repo-owner)
+REPO_NAME=$(curl -f -H Metadata-Flavor:Google ${URL}/repo-name)
+GIT_REF=$(curl -f -H Metadata-Flavor:Google ${URL}/git-ref)
+VERSION=$(curl -f -H Metadata-Flavor:Google ${URL}/version)
+VERSION=${VERSION:="1dummy"}
+
+DEBIAN_FRONTEND=noninteractive
+
+echo "Started build..."
+
+gsutil cp "${SRC_PATH}/common.sh" ./
+. common.sh
+
+try_command apt-get -y update
+try_command apt-get install -y --no-install-{suggests,recommends} git-core \
+  debhelper devscripts build-essential equivs
+
+git_checkout "$REPO_OWNER" "$REPO_NAME" "$GIT_REF"
+
+PKGNAME="$(grep "^Package:" ./packaging/debian/control|cut -d' ' -f2-)"
+
+# Install build deps
+mk-build-deps -t "apt-get -o Debug::pkgProblemResolver=yes \
+  --no-install-recommends --yes" --install packaging/debian/control
+dpkg-checkbuilddeps packaging/debian/control
+
+if grep -q '+deb' packaging/debian/changelog; then
+  DEB=$(</etc/debian_version)
+  DEB="+deb${DEB:0:1}"
+fi
+
+if grep -q 'golang' packaging/debian/control; then
+  echo "Installing go"
+  install_go
+
+  echo "Installing go dependencies"
+  $GO mod download
+fi
+
+echo "Building package(s)"
+
+# Create build dir.
+BUILD_DIR="/tmp/debpackage/"
+[[ -d $BUILD_DIR ]] || mkdir $BUILD_DIR
+
+# Create 'upstream' tarball.
+TAR="${PKGNAME}_${VERSION}.orig.tar.gz"
+tar czvf "${BUILD_DIR}/${TAR}" --exclude .git --exclude packaging \
+  --transform "s/^\./${PKGNAME}-${VERSION}/" .
+
+# Extract tarball and build.
+tar -C "$BUILD_DIR" -xzvf "${BUILD_DIR}/${TAR}"
+PKGDIR="${BUILD_DIR}/${PKGNAME}-${VERSION}"
+cp -r packaging/debian "${BUILD_DIR}/${PKGNAME}-${VERSION}/"
+
+cd "${BUILD_DIR}/${PKGNAME}-${VERSION}"
+
+# We generate this to enable auto-versioning.
+[[ -f debian/changelog ]] && rm debian/changelog
+dch --create -M -v 1:${VERSION}-g1${DEB} --package $PKGNAME -D stable \
+  "Debian packaging for ${PKGNAME}"
+debuild -e "VERSION=${VERSION}" -us -uc
+
+gsutil cp -n "$BUILD_DIR"/*.deb "$GCS_PATH"
+build_success "Built `ls "$BUILD_DIR"/*.deb|xargs`"
---
+++ packagebuild/daisy_startupscript_rpm.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+# Copyright 2019 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This script is expected to be run on an Enterprise Linux system (RHEL, CentOS)
+# on GCE with various flags set in the instance metadata including the git
+# repository to clone. The script produces an RPM defined by an RPM spec in the
+# packaging/ directory from the cloned repo.
+
+URL="http://metadata/computeMetadata/v1/instance/attributes"
+function get_md() {
+  curl -f -H Metadata-Flavor:Google "${URL}/${1}"
+}
+GCS_PATH=$(get_md daisy-outs-path)
+SRC_PATH=$(get_md daisy-sources-path)
+REPO_OWNER=$(get_md repo-owner)
+REPO_NAME=$(get_md repo-name)
+GIT_REF=$(get_md git-ref)
+VERSION=$(get_md version)
+VERSION=${VERSION:-"dummy"}
+
+echo "Started build..."
+
+# common.sh contains functions common to all builds.
+gsutil cp "${SRC_PATH}/common.sh" ./
+. common.sh
+
+# Install git2 as this is not available in centos 6/7
+VERSION_ID=6
+if [[ -f /etc/os-release ]]; then
+  eval $(grep VERSION_ID /etc/os-release)
+  VERSION_ID=${VERSION_ID:0:1}
+fi
+
+GIT="git"
+if [[ ${VERSION_ID} =~ 6|7 ]]; then
+    try_command yum install -y https://rhel${VERSION_ID}.iuscommunity.org/ius-release.rpm
+    rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-IUS-${VERSION_ID}
+    GIT="git2u"
+fi
+
+try_command yum install -y $GIT rpmdevtools yum-utils
+
+git_checkout "$REPO_OWNER" "$REPO_NAME" "$GIT_REF"
+
+if grep -q '%{_go}' ./packaging/*.spec; then
+  echo "Installing go"
+  install_go
+
+  echo "Installing go dependencies"
+  $GO mod download
+fi
+
+# Make build dirs as needed.
+RPMDIR=/usr/src/redhat
+for dir in ${RPMDIR}/{SOURCES,SPECS}; do
+  [[ -d "$dir" ]] || mkdir -p "$dir"
+done
+
+# Find the RPM specs.
+for spec in ./packaging/*.spec; do
+  spec=$(basename "$spec")
+  # If the spec file has elN in it, only add it if N matches VERSION_ID
+  if [[ "$spec" =~ \.el[0-9] ]]; then
+    [[ "$spec" =~ \.el${VERSION_ID} ]] && SPECS="${SPECS} ${spec}"
+  else
+    SPECS="${SPECS} ${spec}"
+  fi
+done
+
+[[ -z "$SPECS" ]] && build_fail "No RPM specs found"
+
+echo "Building package(s)"
+for spec in $SPECS; do
+  PKGNAME="$(grep Name: "./packaging/${spec}"|cut -d' ' -f2-)"
+  yum-builddep "./packaging/${spec}"
+
+  cp "./packaging/${spec}" "${RPMDIR}/SPECS/"
+  tar czvf "${RPMDIR}/SOURCES/${PKGNAME}_${VERSION}.orig.tar.gz" --exclude .git \
+    --exclude packaging --transform "s/^\./${PKGNAME}-${VERSION}/" .
+
+  rpmbuild --define "_topdir ${RPMDIR}/" --define "_version ${VERSION}" \
+    --define "_go ${GO:-""}" --define "_gopath ${GOPATH:-""}" \
+    -ba "${RPMDIR}/SPECS/${spec}"
+done
+
+rpms=$(find ${RPMDIR}/{S,}RPMS -iname "${PKGNAME}*.rpm")
+gsutil cp -n ${rpms} "$GCS_PATH"
+build_success "Built ${rpms}"
