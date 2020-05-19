1e8458b6d67978b5bfab23456b4384ead09ecc06
Liam Hopkins <liamh@google.com>
2020-05-19 15:48:36 -0700

    add flake8 runner

---
+++ container_images/flake8/main.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Copyright 2020 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+python -V
+flake8 --version
+flake8 --show-source --ignore E111,E114,E121,E125,E128,E129,W503 --import-order-style=google ./
+RET=$?
+
+# Print results and return.
+if [ ${RET} != 0 ]; then
+  echo "flake8 returned ${RET}"
+  exit 1
+fi
+exit 0
