259b7c48c658490ed8263844d4fc58f9898fbf48
Han Gao <hanga@google.com>
2021-04-06 18:52:15 +0000

    address comments

--- container_images/test-manager/build.sh
+++ container_images/test-manager/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Copyright 2017 Google Inc. All Rights Reserved.
+# Copyright 2021 Google Inc. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,39 +13,28 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ValidateReturn() {
-  RET=$?
-  if [ $RET -ne 0 ]; then
-    GOBUILD_OUT=$RET
-    echo "'go build or go test -c' exited with ${GOBUILD_OUT}"
-  else
-    echo "'go build or go test -c' success"
-  fi
-}
-
 export CGO_ENABLED=0 GOOS=linux
 BUILD_DIR=./test_manager
-chmod o+x -R $BUILD_DIR
-[ -n "$BUILD_DIR" ] && cd $BUILD_DIR
+cd $BUILD_DIR
 mkdir /out
 
 GOBUILD_OUT=0
 
 echo "Start Building"
-for d in ./*; do
+for d in *; do
   d=`basename $d`
-  if [ "${d}" = "test_suites" ];then
-    for test_suite in ${d}/*; do
-      test_suite=`basename ${test_suite}`
-      go test -c ./test_suites/${test_suite} -o /out/${test_suite}.test
-      ValidateReturn
-    done
-  else
-    pushd ${d} || continue
-    go get && go build -v -o /out/${d}
-    ValidateReturn
+  if [[ "$d" != "test_suites" ]]; then
+    pushd $d || continue
+    go get && go build -v -o /out/$d
+    echo "'go build' exited with $?"
     popd
   fi
+done
+
+for test_suite in ./test_suites; do
+  test_suite=`basename ${test_suite}`
+  go test -c ./test_suites/${test_suite} -o /out/${test_suite}.test
+  echo "'go test -c' exited with $?"
 done
 
 echo "All build output:"
