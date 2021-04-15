5815f1c863dcdd3202ef28469fb93cb2a5b4ca20
Han Gao <hanga@google.com>
2021-04-15 02:13:18 +0000

    add image boot test

--- imagetest/cmd/manager/main.go
+++ imagetest/cmd/manager/main.go
@@ -8,6 +8,7 @@ import (
 	"strings"
 
 	"github.com/GoogleCloudPlatform/guest-test-infra/imagetest"
+	"github.com/GoogleCloudPlatform/guest-test-infra/imagetest/test_suites/image_boot"
 	imagevalidation "github.com/GoogleCloudPlatform/guest-test-infra/imagetest/test_suites/image_validation"
 )
 
@@ -45,6 +46,10 @@ func main() {
 		{
 			imagevalidation.Name,
 			imagevalidation.TestSetup,
+		},
+		{
+			image_boot.Name,
+			image_boot.TestSetup,
 		},
 	}
 
