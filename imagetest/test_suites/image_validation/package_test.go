d97b277fd5398ba9538ca190af780d60d9518cff
Han Gao <hanga@google.com>
2021-04-30 23:35:06 +0000

    add test gcloud gsutil

---
+++ imagetest/test_suites/image_validation/package_test.go
@@ -0,0 +1,21 @@
+package imagevalidation
+
+import (
+	"os/exec"
+	"testing"
+)
+
+func TestStandardPrograms(t *testing.T) {
+	cmd := exec.Command("gcloud", "-h")
+	cmd.Start()
+	err := cmd.Wait()
+	if err != nil {
+		t.Fatalf("gcloud not installed properly")
+	}
+	cmd = exec.Command("gsutil", "help")
+	cmd.Start()
+	err = cmd.Wait()
+	if err != nil {
+		t.Fatalf("gsutil not installed properly")
+	}
+}
