bb6be4b23421a61cc5762795ae4a75c4378d2b65
Han Gao <hanga@google.com>
2021-06-02 16:48:36 -0700

    add security image test (#132)
    * add security image test
    
    * fix regex dot match new lin,
    e
    
    * fix regex
    
    * address comments
    
    * small string change
    
    * address comments
    
    * fix various bugs
    
    * check err instead stdout
    
    * address comments, refactor
    
    * fix apt config
    
    * format
    
    * fix debian-9
    
    * address comments
    
    * refactor
    
    * Update image_security_test.go
    
    Co-authored-by: Liam Hopkins <liamh@google.com>

--- imagetest/cmd/manager/main.go
+++ imagetest/cmd/manager/main.go
@@ -14,6 +14,7 @@ import (
 	imageboot "github.com/GoogleCloudPlatform/guest-test-infra/imagetest/test_suites/image_boot"
 	imagevalidation "github.com/GoogleCloudPlatform/guest-test-infra/imagetest/test_suites/image_validation"
 	"github.com/GoogleCloudPlatform/guest-test-infra/imagetest/test_suites/network"
+	"github.com/GoogleCloudPlatform/guest-test-infra/imagetest/test_suites/security"
 )
 
 var (
@@ -116,6 +117,10 @@ func main() {
 		{
 			network.Name,
 			network.TestSetup,
+		},
+		{
+			security.Name,
+			security.TestSetup,
 		},
 	}
 
