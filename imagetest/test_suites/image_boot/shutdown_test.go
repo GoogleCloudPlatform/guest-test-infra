f2ff41c36e4ec2976d52a085feedf7cafea206e3
Han Gao <ghnankai@qq.com>
2021-06-23 14:46:48 -0700

    Update imagetest/test_suites/image_boot/shutdown_test.go
    Co-authored-by: Liam Hopkins <liamh@google.com>

--- imagetest/test_suites/image_boot/shutdown_test.go
+++ imagetest/test_suites/image_boot/shutdown_test.go
@@ -11,14 +11,6 @@ const shutdownTime = 110 // about 2 minutes
 
 // TestGuestShutdownScript test that shutdown scripts can run for around two minutes
 func TestGuestShutdownScript(t *testing.T) {
-	_, err := os.Stat(markerFile)
-	if os.IsNotExist(err) {
-		// first boot
-		if _, err := os.Create(markerFile); err != nil {
-			t.Fatalf("failed creating marker file: %v", err)
-		}
-		t.Fatal("marker file does not exist")
-	}
 	// second boot
 	bytes, err := ioutil.ReadFile("/shutdown.txt")
 	if err != nil {
