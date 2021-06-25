46218b8e410dcade7bacb0d086e6708e0ccb222c
Zach Marano <zmarano@google.com>
2021-06-25 12:25:18 -0700

    Skip secure boot tests for Debian 9.

--- imagetest/test_suites/image_boot/setup.go
+++ imagetest/test_suites/image_boot/setup.go
@@ -1,6 +1,10 @@
 package imageboot
 
-import "github.com/GoogleCloudPlatform/guest-test-infra/imagetest"
+import (
+	"strings"
+
+	"github.com/GoogleCloudPlatform/guest-test-infra/imagetest"
+)
 
 // Name is the name of the test package. It must match the directory name.
 var Name = "image_boot"
@@ -24,6 +28,10 @@ func TestSetup(t *imagetest.TestWorkflow) error {
 		return err
 	}
 	vm.RunTests("TestGuestBoot|TestGuestReboot|TestGuestShutdownScript")
+
+	if strings.Contains(t.Image, "debian-9") {
+		t.Skip("secure boot is not supported on Debian 9")
+	}
 
 	vm2, err := t.CreateTestVM("vm2")
 	if err != nil {
