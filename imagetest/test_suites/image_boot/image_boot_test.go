02de418668e967feac27e967bb0cc80b2933a3c3
Zach Marano <zmarano@google.com>
2021-07-09 13:55:24 -0700

    Switch to modern efivars file. (#182)
    * Switch to modern efivars file.
    
    * fix

--- imagetest/test_suites/image_boot/image_boot_test.go
+++ imagetest/test_suites/image_boot/image_boot_test.go
@@ -16,7 +16,7 @@ var (
 
 const (
 	markerFile     = "/boot-marker"
-	secureBootFile = "/sys/firmware/efi/vars/SecureBoot-8be4df61-93ca-11d2-aa0d-00e098032b8c/data"
+	secureBootFile = "/sys/firmware/efi/efivars/SecureBoot-8be4df61-93ca-11d2-aa0d-00e098032b8c"
 )
 
 func TestMain(m *testing.M) {
@@ -67,7 +67,7 @@ func TestGuestSecureBoot(t *testing.T) {
 		t.Fatal("failed reading secure boot file")
 	}
 	// https://www.kernel.org/doc/Documentation/ABI/stable/sysfs-firmware-efi-vars
-	if data[0] != 1 {
+	if data[len(data)-1] != 1 {
 		t.Fatal("secure boot is not enabled as expected")
 	}
 }
