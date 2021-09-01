65d7263b4d867217763d9b2cc32faddb23083390
Dave Linskey <linskeyd@google.com>
2021-09-01 22:15:08 +0000

    add cli utility for publishing a cloud monitoring time series metric data point

---
+++ container_images/concourse-metrics/pkg/monitoring/requests.go
@@ -0,0 +1,119 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package monitoring
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/golang/protobuf/ptypes/timestamp"
+	metricpb "google.golang.org/genproto/googleapis/api/metric"
+	"google.golang.org/genproto/googleapis/api/monitoredres"
+	monitoringpb "google.golang.org/genproto/googleapis/monitoring/v3"
+)
+
+type JobResultArgs struct {
+	EndTimestamp   *int64
+	Job            string
+	MetricPath     string
+	Pipeline       string
+	ProjectId      string
+	ResultState    string
+	StartTimestamp int64
+	Task           string
+	Zone           string
+}
+
+func validateJobResultRequestInput(input *JobResultArgs) error {
+	if strings.TrimSpace(input.ProjectId) == "" {
+		return fmt.Errorf("empty project-id value")
+	}
+	if strings.TrimSpace(input.Zone) == "" {
+		return fmt.Errorf("empty zone value")
+	}
+	if strings.TrimSpace(input.Pipeline) == "" {
+		return fmt.Errorf("empty pipeline value")
+	}
+	if strings.TrimSpace(input.Job) == "" {
+		return fmt.Errorf("empty job value")
+	}
+	if strings.TrimSpace(input.Task) == "" {
+		return fmt.Errorf("empty task value")
+	}
+	if strings.TrimSpace(input.MetricPath) == "" {
+		return fmt.Errorf("empty metric-path value")
+	}
+	state := strings.TrimSpace(input.ResultState)
+	if state != "success" && state != "failure" {
+		return fmt.Errorf("invalid state value")
+	}
+
+	// Don't let the end timestamp occur before the start timestamp.
+	if input.EndTimestamp != nil && *input.EndTimestamp < input.StartTimestamp {
+		return fmt.Errorf("end-timestamp cannot occur before start-timestamp")
+	}
+
+	return nil
+}
+
+func BuildJobResultRequest(input JobResultArgs) (*monitoringpb.CreateTimeSeriesRequest, error) {
+	// Provide a default for the endTimestamp if one was not provided.
+	if input.EndTimestamp == nil {
+		*input.EndTimestamp = time.Now().UnixMilli()
+	}
+
+	e := validateJobResultRequestInput(&input)
+	if e != nil {
+		return nil, e
+	}
+
+	// Calculate the duration to publish.
+	duration := *input.EndTimestamp - input.StartTimestamp
+
+	return &monitoringpb.CreateTimeSeriesRequest{
+		Name: "projects/" + input.ProjectId,
+		TimeSeries: []*monitoringpb.TimeSeries{{
+			Metric: &metricpb.Metric{
+				Type: "custom.googleapis.com/" + input.MetricPath,
+				Labels: map[string]string{
+					"result_state": input.ResultState,
+				},
+			},
+			Resource: &monitoredres.MonitoredResource{
+				Type: "generic_task",
+				Labels: map[string]string{
+					"project_id": input.ProjectId,
+					"location":   input.Zone,
+					"namespace":  input.Pipeline,
+					"job":        input.Job,
+					"task_id":    input.Task,
+				},
+			},
+			Points: []*monitoringpb.Point{{
+				Interval: &monitoringpb.TimeInterval{
+					EndTime: &timestamp.Timestamp{
+						Seconds: *input.EndTimestamp / 1000,
+					},
+				},
+				Value: &monitoringpb.TypedValue{
+					Value: &monitoringpb.TypedValue_Int64Value{
+						Int64Value: duration,
+					},
+				},
+			}},
+		}},
+	}, nil
+}
---
+++ container_images/concourse-metrics/pkg/monitoring/requests_test.go
@@ -0,0 +1,96 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package monitoring
+
+import (
+	"reflect"
+	"strings"
+	"testing"
+)
+
+const (
+	job            = "test-job"
+	metricPath     = "metric-path"
+	pipeline       = "test-pipeline"
+	projectId      = "test-project"
+	resultState    = "success"
+	startTimestamp = 1620363558000
+	task           = "test-task"
+	zone           = "test-zone"
+)
+
+func TestBuildJobResultRequest(t *testing.T) {
+	// Instantiated since you can't make pointers to int literals (since they are not addressable).
+	validEndTimestamp := int64(1620363568000)
+
+	input := JobResultArgs{
+		EndTimestamp:   &validEndTimestamp,
+		Job:            job,
+		MetricPath:     metricPath,
+		Pipeline:       pipeline,
+		ProjectId:      projectId,
+		ResultState:    resultState,
+		StartTimestamp: startTimestamp,
+		Task:           task,
+		Zone:           zone,
+	}
+	result, err := BuildJobResultRequest(input)
+	if err != nil {
+		t.Errorf("Happy path BuildJobResultRequest should not return an error: %+v", err)
+	}
+
+	assertEqual(t, result.Name, "projects/"+projectId)
+	assertEqual(t, result.TimeSeries[0].Metric.Type, "custom.googleapis.com/"+metricPath)
+	assertEqual(t, result.TimeSeries[0].Metric.Labels["result_state"], resultState)
+	assertEqual(t, result.TimeSeries[0].Resource.Type, "generic_task")
+	assertEqual(t, result.TimeSeries[0].Resource.Labels["project_id"], projectId)
+	assertEqual(t, result.TimeSeries[0].Resource.Labels["location"], zone)
+	assertEqual(t, result.TimeSeries[0].Resource.Labels["namespace"], pipeline)
+	assertEqual(t, result.TimeSeries[0].Resource.Labels["job"], job)
+	assertEqual(t, result.TimeSeries[0].Resource.Labels["task_id"], task)
+	assertEqual(t, result.TimeSeries[0].Points[0].Interval.EndTime.Seconds, validEndTimestamp/1000)
+	assertEqual(t, result.TimeSeries[0].Points[0].Value.GetInt64Value(), validEndTimestamp-startTimestamp)
+}
+
+func TestBuildJobResultRequestInputValidation(t *testing.T) {
+	// Instantiated since you can't make pointers to int literals (since they are not addressable).
+	validEndTimestamp := int64(1620363568000)
+
+	tests := []struct {
+		args    JobResultArgs
+		message string
+	}{
+		{JobResultArgs{EndTimestamp: &validEndTimestamp, MetricPath: metricPath, StartTimestamp: startTimestamp, Job: job, Pipeline: pipeline, ProjectId: projectId, ResultState: resultState, Task: task, Zone: " "}, "empty zone"},
+		{JobResultArgs{EndTimestamp: &validEndTimestamp, MetricPath: metricPath, StartTimestamp: startTimestamp, Job: job, Pipeline: pipeline, ProjectId: projectId, ResultState: resultState, Task: " ", Zone: zone}, "empty task"},
+		{JobResultArgs{EndTimestamp: &validEndTimestamp, MetricPath: metricPath, StartTimestamp: startTimestamp, Job: job, Pipeline: pipeline, ProjectId: projectId, ResultState: "other", Task: task, Zone: zone}, "invalid state"},
+		{JobResultArgs{EndTimestamp: &validEndTimestamp, MetricPath: metricPath, StartTimestamp: startTimestamp, Job: job, Pipeline: pipeline, ProjectId: " ", ResultState: resultState, Task: task, Zone: zone}, "empty project"},
+		{JobResultArgs{EndTimestamp: &validEndTimestamp, MetricPath: metricPath, StartTimestamp: startTimestamp, Job: job, Pipeline: " ", ProjectId: projectId, ResultState: resultState, Task: task, Zone: zone}, "empty pipeline"},
+		{JobResultArgs{EndTimestamp: &validEndTimestamp, MetricPath: metricPath, StartTimestamp: startTimestamp, Job: " ", Pipeline: pipeline, ProjectId: projectId, ResultState: resultState, Task: task, Zone: zone}, "empty job"},
+		{JobResultArgs{EndTimestamp: &validEndTimestamp, MetricPath: metricPath, StartTimestamp: validEndTimestamp + 1000, Job: job, Pipeline: pipeline, ProjectId: projectId, ResultState: resultState, Task: task, Zone: zone}, "end-timestamp cannot occur before start-timestamp"},
+	}
+	for _, test := range tests {
+		if _, err := BuildJobResultRequest(test.args); err == nil || !strings.Contains(err.Error(), test.message) {
+			t.Errorf("BuildJobResultRequest(%+v) = (_, %v); want error to contain %s", test.args, err, test.message)
+		}
+	}
+}
+
+func assertEqual(t *testing.T, x interface{}, y interface{}) {
+	if x == y {
+		return
+	}
+
+	t.Errorf("Received %v (type %v), expected %v (type %v)", x, reflect.TypeOf(x), y, reflect.TypeOf(y))
+}
