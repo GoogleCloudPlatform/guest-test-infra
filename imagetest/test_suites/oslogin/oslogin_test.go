264b9a20c0e9bfade78a6844a93c7ab9a8fb2d55
Rick Wright <rickw@google.com>
2021-11-02 14:22:26 -0700

    Add OS Login tests for getent passwd.

--- imagetest/test_suites/oslogin/oslogin_test.go
+++ imagetest/test_suites/oslogin/oslogin_test.go
@@ -5,9 +5,14 @@ package oslogin
 
 import (
 	"io/ioutil"
+	"os/exec"
 	"strings"
 	"testing"
 )
+
+const TEST_USERNAME = "sa_105020877179577573373"
+const TEST_UID = "3651018652"
+const TEST_USER_ENTRY = TEST_USERNAME + ":*:" + TEST_UID + ":" + TEST_UID + "::/home/" + TEST_USERNAME
 
 func TestOsLoginEnabled(t *testing.T) {
 	// Check OS Login enabled in /etc/nsswitch.conf
@@ -95,4 +100,60 @@ func TestOsLoginDisabled(t *testing.T) {
 	if strings.Contains(contents, "pam_oslogin_login.so") || strings.Contains(contents, "pam_oslogin_admin.so") {
 		t.Errorf("OS Login PAM module wrongly included in pam.d/sshd when disabled.")
 	}
+}
+
+func TestGetentPasswdOsloginUser(t *testing.T) {
+	out := RunGetent(t, "passwd", TEST_USERNAME)
+	if !strings.Contains(out, TEST_USER_ENTRY) {
+		t.Errorf("getent passwd output does not contain %s", TEST_USER_ENTRY)
+	}
+}
+
+func TestGetentPasswdAllUsers(t *testing.T) {
+	out := RunGetent(t, "passwd")
+	if !strings.Contains(out, "root:x:0:0:root:/root:") {
+		t.Errorf("getent passwd output does not contain user root")
+	}
+	if !strings.Contains(out, "nobody:x:") {
+		t.Errorf("getent passwd output does not contain user nobody")
+	}
+	if !strings.Contains(out, TEST_USER_ENTRY) {
+		t.Errorf("getent passwd output does not contain %s", TEST_USER_ENTRY)
+	}
+}
+
+func TestGetentPasswdOsloginUID(t *testing.T) {
+	out := RunGetent(t, "passwd", TEST_UID)
+	if !strings.Contains(out, TEST_USER_ENTRY) {
+		t.Errorf("getent passwd output does not contain %s", TEST_USER_ENTRY)
+	}
+}
+
+func TestGetentPasswdLocalUser(t *testing.T) {
+	out := RunGetent(t, "passwd", "nobody")
+	if !strings.Contains(out, "nobody:x:") {
+		t.Errorf("getent passwd output does not contain user nobody")
+	}
+}
+
+func TestGetentPasswdInvalidUser(t *testing.T) {
+	err := RunGetentErr(t, "passwd", "__invalid_user___")
+	if err.Error() != "exit status 2" {
+		t.Errorf("getent passwd did not give error on invaid user")
+	}
+}
+
+func RunGetent(t *testing.T, args ...string) string {
+	cmd := exec.Command("/usr/bin/getent", args...)
+	out, err := cmd.Output()
+	if err != nil {
+		t.Fatalf("getent command failed %v", err)
+	}
+	return string(out)
+}
+
+func RunGetentErr(t *testing.T, args ...string) error {
+	cmd := exec.Command("/usr/bin/getent", args...)
+	err := cmd.Run()
+	return err
 }
