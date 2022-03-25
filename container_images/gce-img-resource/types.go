14f075972aab931ca23691de9ffbe887e420ddda
Liam Hopkins <liamh@google.com>
2022-03-25 14:01:20 -0700

    update comment

--- container_images/gce-img-resource/types.go
+++ container_images/gce-img-resource/types.go
@@ -6,13 +6,14 @@ type Source struct {
 	// Family limits matching images to those with the specified family. Optional.
 	Family string `json:"family"`
 	// Regexp defines a regular expression to find versions embedded in image names. Optional.
-	//Regexp string `json:"regexp"` // TODO: Not yet implemented.
+	Regexp string `json:"regexp"` // TODO: Not yet implemented.
 }
 
 // Version represents a single image version.
 type Version struct {
+	// Name is used to determine the image in a `get` step.
 	Name string `json:"name"`
-	// Version is used for ordering returned images. Defaults to the image creation timestamp. If regexp is
-	// specified, Version will instead contain the parsed version.
+	// Version is used for ordering returned images in `check` steps. Defaults to the image creation timestamp.
+	// If regexp is specified, Version will instead contain the parsed version.
 	Version string `json:"version,omitempty"`
 }
