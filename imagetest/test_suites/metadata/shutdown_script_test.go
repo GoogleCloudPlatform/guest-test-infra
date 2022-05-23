0c7bdd242e9ff9d3a96534d76790b1345554720c
Tony Zhang <33922283+dntczdx@users.noreply.github.com>
2022-05-23 15:07:56 -0700

    [guest-agent image test] fix: verify that shut down script executed no less than 80s (#474)
    * [guest-agent image test] verify that shut down script executed no less than 80s
    
    * don't change file name

--- imagetest/test_suites/metadata/shutdown_script_test.go
+++ imagetest/test_suites/metadata/shutdown_script_test.go
@@ -10,7 +10,8 @@ import (
 	"github.com/GoogleCloudPlatform/guest-test-infra/imagetest/utils"
 )
 
-const shutdownTime = 110 // about 2 minutes
+// The designed shutdown limit is 90s. Let's verify it's executed no less than 80s.
+const shutdownTime = 80
 
 // TestShutdownScript test the standard metadata script.
 func TestShutdownScript(t *testing.T) {
