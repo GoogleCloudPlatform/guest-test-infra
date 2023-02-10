8a8260124482a6d5505797390bd7d7f50911d4ac
Leandro Dorileo <dorileo@google.com>
2023-02-09 17:24:02 -0800

    rpm build: make sure we have python3-devel available (#599)
    Without the python3-devel we'll not have the python3 rpm macros available
    breaking packages requiring them.

--- packagebuild/daisy_startupscript_rpm.sh
+++ packagebuild/daisy_startupscript_rpm.sh
@@ -62,7 +62,7 @@ if [[ ${VERSION_ID} = 9 ]]; then
   fi
 fi
 
-try_command yum install -y $GIT rpmdevtools yum-utils
+try_command yum install -y $GIT rpmdevtools yum-utils python3-devel
 
 git_checkout "$REPO_OWNER" "$REPO_NAME" "$GIT_REF"
 
