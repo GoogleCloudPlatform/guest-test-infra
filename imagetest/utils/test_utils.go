459531b3a92c32ff0b4617b2271f083cf02215fb
Anthony Lu <koln@google.com>
2023-07-25 21:41:33 +0000

    go fmt

--- imagetest/utils/test_utils.go
+++ imagetest/utils/test_utils.go
@@ -255,7 +255,7 @@ func CheckLinuxCmdExists(cmd string) bool {
 	cmdPath, err := exec.LookPath(cmd)
 	// returns nil prior to go 1.19, exec.ErrDot after
 	if errors.Is(err, exec.ErrDot) || err == nil {
-		cmdFileMode, err :=  os.Stat(cmdPath)
+		cmdFileMode, err := os.Stat(cmdPath)
 		// check the bitmask if the owner, group, or others can execute the file.
 		if err == nil {
 			return mode&0111 != 0
