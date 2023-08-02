5896b71d1419091397e8a92922a8d539f4465d06
koln67 <koln@google.com>
2023-08-02 21:36:38 +0000

    Create test vm with multiple disks in fixtures

--- imagetest/fixtures.go
+++ imagetest/fixtures.go
@@ -82,7 +82,7 @@ func (t *TestWorkflow) CreateTestVM(name string) (*TestVM, error) {
 		return nil, err
 	}
 
-	// createDisksStep doesn't depend on any other steps.
+	// createDisksStep doesn't depend on any other steps
 	createVMStep, i, err := t.appendCreateVMStep([]*compute.Disk{bootDisk}, name)
 	if err != nil {
 		return nil, err
@@ -90,6 +90,62 @@ func (t *TestWorkflow) CreateTestVM(name string) (*TestVM, error) {
 
 	if err := t.wf.AddDependency(createVMStep, createDisksStep); err != nil {
 		return nil, err
+	}
+
+	waitStep, err := t.addWaitStep(vmname, vmname, false)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := t.wf.AddDependency(waitStep, createVMStep); err != nil {
+		return nil, err
+	}
+
+	if createSubnetworkStep, ok := t.wf.Steps[createSubnetworkStepName]; ok {
+		if err := t.wf.AddDependency(createVMStep, createSubnetworkStep); err != nil {
+			return nil, err
+		}
+	}
+
+	if createNetworkStep, ok := t.wf.Steps[createNetworkStepName]; ok {
+		if err := t.wf.AddDependency(createVMStep, createNetworkStep); err != nil {
+			return nil, err
+		}
+	}
+
+	return &TestVM{name: vmname, testWorkflow: t, instance: i}, nil
+}
+
+// CreateTestVM adds the necessary steps to create a VM with the specified
+// name to the workflow.
+func (t *TestWorkflow) CreateTestVMMultipleDisks(disks []*compute.Disk) (*TestVM, error) {
+	if len(disks) == 0 || disks[0].Name == "" {
+		return nil, fmt.Errorf("failed to create multiple disk VM with empty boot disk")
+	}
+
+	name := disks[0].Name
+	parts := strings.Split(name, ".")
+	vmname := strings.ReplaceAll(parts[0], "_", "-")
+
+	createDisksSteps := make([]*daisy.Step, len(disks))
+	for i, disk := range disks {
+		createDisksStep, err := t.appendCreateDisksStep(disk)
+		if err != nil {
+			return nil, err
+		}
+		createDisksSteps[i] = createDisksStep
+	}
+
+	// createDisksStep doesn't depend on any other steps
+	createVMStep, i, err := t.appendCreateVMStep(disks, name)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, createDisksStep := range createDisksSteps {
+		if err := t.wf.AddDependency(createVMStep, createDisksStep); err != nil {
+			return nil, err
+		}
 	}
 
 	waitStep, err := t.addWaitStep(vmname, vmname, false)
