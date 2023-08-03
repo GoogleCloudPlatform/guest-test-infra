d76a416ea50149d0dced4b7b7d45bf1e60614b09
koln67 <koln@google.com>
2023-08-03 21:54:45 +0000

    Fix storage_perf segmentation fault

--- imagetest/test_suites/storage_perf/setup.go
+++ imagetest/test_suites/storage_perf/setup.go
@@ -7,7 +7,16 @@ import (
 // Name is the name of the test package. It must match the directory name.
 var Name = "storage_perf"
 
+const (
+	vmName = "vm"
+)
+
 // TestSetup sets up the test workflow.
 func TestSetup(t *imagetest.TestWorkflow) error {
+	vm, err := t.CreateTestVM(vmName)
+	if err != nil {
+		return err
+	}
+	vm.UseGVNIC()
 	return nil
 }
