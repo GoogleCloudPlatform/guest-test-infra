1d81c66b304ea25b0990c61e01c88c20d2c5d36e
drewhli <andrewhl@google.com>
2023-08-08 18:19:09 +0000

    Update the startupscripts

--- imagetest/test_suites/network_perf/startupscripts/netclient_startup.sh
+++ imagetest/test_suites/network_perf/startupscripts/netclient_startup.sh
@@ -6,6 +6,7 @@
 vmname=$(curl http://metadata.google.internal/computeMetadata/v1/instance/hostname -H "Metadata-Flavor: Google" | cut -d"." -f1)
 outfile=$(curl http://metadata.google.internal/computeMetadata/v1/instance/hostname -H "Metadata-Flavor: Google" | cut -d"." -f1).txt
 iperftarget=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/iperftarget -H "Metadata-Flavor: Google")
+sleepduration=60
 
 echo "MTU: "
 /sbin/ifconfig | grep mtu | tee -a "$outfile"
@@ -17,26 +18,24 @@ elif [[ -f /bin/dnf ]]; then
   echo "$(date +"%Y-%m-%d %T"): dnf found Installing iperf." | tee -a "$outfile"
   os=$(cat /etc/redhat-release)
   if [[ "$os" == *"release 9."* ]]; then
-    sudo dnf -y install https://dl.fedoraproject.org/pub/epel/9/Everything/x86_64/Packages/i/iperf-2.1.6-2.el9.x86_64.rpm | tee -a "$outfile"
+    sudo dnf -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm | tee -a "$outfile"
   fi
   if [[ "$os" == *"release 8."* ]]; then
-    sudo dnf -y install https://dl.fedoraproject.org/pub/epel/8/Everything/x86_64/Packages/i/iperf-2.1.6-2.el8.x86_64.rpm | tee -a "$outfile"
+    sudo dnf -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm | tee -a "$outfile"
   fi
-  sudo dnf -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm | tee -a "$outfile"
-  sudo dnf -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm | tee -a "$outfile"
   sudo sudo dnf makecache && sudo dnf -y install iperf | tee -a "$outfile"
-elif [[ ! -f /bin/dnf ]] && [[ -f /bin/yum ]]; then
+elif [[ -f /bin/yum ]]; then
   echo "$(date +"%Y-%m-%d %T"): yum found Installing iperf." | tee -a "$outfile"
   yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm | tee -a "$outfile"
   sudo sudo yum makecache && sudo yum -y install iperf | tee -a "$outfile"
 elif [[ -f /usr/bin/zypper ]]; then
   echo "$(date +"%Y-%m-%d %T"): zypper found Installing iperf." | tee -a "$outfile"
   sudo zypper --no-gpg-checks refresh | tee -a "$outfile"
-  sudo zypper --no-gpg-checks --non-interactive install https://iperf.fr/download/opensuse/iperf-2.0.5-14.1.2.x86_64.rpm | tee -a "$outfile"
+  sudo zypper --no-gpg-checks --non-interactive install iperf | tee -a "$outfile"
 fi
 
-# Wait 60 seconds for the server VM to start up iperf server.
-sleep 60
+# Wait for the server VM to start up iperf server.
+sleep "$sleepduration"
 
 echo "$(date +"%Y-%m-%d %T"): Running iperf client with target $iperftarget. iperf version: $(iperf -v)" | tee -a "$outfile"
 iperf -t 30 -c "$iperftarget" -P 16 | tee -a "$outfile"
--- imagetest/test_suites/network_perf/startupscripts/netserver_startup.sh
+++ imagetest/test_suites/network_perf/startupscripts/netserver_startup.sh
@@ -12,22 +12,20 @@ elif [[ -f /bin/dnf ]]; then
   echo "dnf found Installing iperf."
   os=$(cat /etc/redhat-release)
   if [[ "$os" == *"release 9."* ]]; then
-    sudo dnf -y install https://dl.fedoraproject.org/pub/epel/9/Everything/x86_64/Packages/i/iperf-2.1.6-2.el9.x86_64.rpm
+    sudo dnf -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
   fi
   if [[ "$os" == *"release 8."* ]]; then
-    sudo dnf -y install https://dl.fedoraproject.org/pub/epel/8/Everything/x86_64/Packages/i/iperf-2.1.6-2.el8.x86_64.rpm
+    sudo dnf -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
   fi
-  sudo dnf -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
-  sudo dnf -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
   sudo sudo dnf makecache && sudo dnf -y install iperf
-elif [[ ! -f /bin/dnf ]] && [[ -f /bin/yum ]]; then
+elif [[ -f /bin/yum ]]; then
   echo "yum found Installing iperf."
   yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
   sudo sudo yum makecache && sudo yum -y install iperf
 elif [[ -f /usr/bin/zypper ]]; then
   echo "zypper found Installing iperf."
   sudo zypper --no-gpg-checks refresh
-  sudo zypper --no-gpg-checks --non-interactive install https://iperf.fr/download/opensuse/iperf-2.0.5-14.1.2.x86_64.rpm
+  sudo zypper --no-gpg-checks --non-interactive install iperf
 fi
 
 echo "Starting iperf server. iperf version: $(iperf -v)"
