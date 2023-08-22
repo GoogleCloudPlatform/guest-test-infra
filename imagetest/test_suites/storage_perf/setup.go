27c85841e67afd4248229c5a4bc5db91ea0975b9
Anthony Lu <67125539+koln67@users.noreply.github.com>
2023-08-22 10:38:09 -0700

    Pass in machine type to fixtures (#754)
    * Pass in machine type to fixtures
    
    * gofmt

--- imagetest/test_suites/storage_perf/setup.go
+++ imagetest/test_suites/storage_perf/setup.go
@@ -15,8 +15,9 @@ func TestSetup(t *imagetest.TestWorkflow) error {
 	if bootdiskSize == hyperdiskSize {
 		return fmt.Errorf("boot disk and mount disk must be different sizes for disk identification")
 	}
+	hyperdiskParams := map[string]string{"machineType": "c3-standard-88"}
 	vm, err := t.CreateTestVMMultipleDisks([]*compute.Disk{{Name: vmName, Type: imagetest.PdBalanced, SizeGb: bootdiskSize},
-		{Name: mountDiskName, Type: imagetest.HyperdiskExtreme, SizeGb: hyperdiskSize}})
+		{Name: mountDiskName, Type: imagetest.HyperdiskExtreme, SizeGb: hyperdiskSize}}, hyperdiskParams)
 	if err != nil {
 		return err
 	}
