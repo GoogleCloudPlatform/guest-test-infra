bc182a622b5746cd97d4485c4168993b58803167
koln67 <koln@google.com>
2023-08-23 22:39:56 +0000

    use dnf first instead of yum

--- imagetest/test_suites/storage_perf/startupscripts/install_fio.sh
+++ imagetest/test_suites/storage_perf/startupscripts/install_fio.sh
@@ -1,16 +1,9 @@
 if [[ -f /usr/bin/apt ]]; then
-	sudo apt -y update && sudo apt -y install fio
-fi
-
-if [[ -f /bin/yum ]]; then
-	sudo yum -y install fio
-fi
-
-if [[ -f /usr/bin/zypper ]]; then
+	apt -y update && apt -y install fio
+elif [[ -f /bin/dnf ]]; then 
+	dnf -y install fio
+elif [[ -f /bin/yum ]]; then
+	yum -y install fio
+elif [[ -f /usr/bin/zypper ]]; then
 	sudo zypper --non-interactive install fio
 fi
-
-if [[ -f /bin/dnf ]]; then
-	sudo dnf -y install fio
-fi
-
