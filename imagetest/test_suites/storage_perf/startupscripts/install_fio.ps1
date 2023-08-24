4ab9492f18b466942f2ad1c07bb23498a604a339
koln67 <koln@google.com>
2023-08-24 19:24:00 +0000

    Startup script changes

--- imagetest/test_suites/storage_perf/startupscripts/install_fio.ps1
+++ imagetest/test_suites/storage_perf/startupscripts/install_fio.ps1
@@ -1,3 +1,9 @@
 cd C:\
 gsutil cp gs://gce-image-build-resources/windows/fio-3.35-x64.msi C:\
-msiexec.exe /A "C:\fio-3.35-x64.msi" /quiet /norestart
+try {
+	msiexec.exe "C:\fio-3.35-x64.msi" /quiet /norestart
+}
+catch {
+	Write-Output "msi file failed to resolve"
+	Write-Output $_
+}
--- imagetest/test_suites/storage_perf/startupscripts/install_fio.sh
+++ imagetest/test_suites/storage_perf/startupscripts/install_fio.sh
@@ -6,4 +6,10 @@ elif [[ -f /bin/yum ]]; then
 	yum -y install fio
 elif [[ -f /usr/bin/zypper ]]; then
 	zypper --non-interactive install fio
+else 
+	echo "No package managers found to install fio"
 fi
+
+errorcode=$?
+if [[ errorcode != 0 ]]; then
+	echo "Error running install fio command: code $errorcode"
