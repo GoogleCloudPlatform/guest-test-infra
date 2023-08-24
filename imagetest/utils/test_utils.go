c4cee541be7b856cd057dcf12f8388517380e123
James Jerger <35977367+jjerger@users.noreply.github.com>
2023-08-24 12:41:58 -0700

    Add IsWindows to test utils
    Simplifies having to import and run runtime.GOOS in every test suite targeting windows and linux.

--- imagetest/utils/test_utils.go
+++ imagetest/utils/test_utils.go
@@ -289,6 +289,14 @@ func Skip32BitWindows(t *testing.T, skipMsg string) {
 	}
 }
 
+// IsWindows returns true if the detected runtime environment is Windows.
+func IsWindows() bool {
+	if runtime.GOOS == "windows" {
+		return true
+	}
+	return false
+}
+
 // IsWindowsClient returns true if the image is a client (non-server) Windows image.
 func IsWindowsClient(image string) bool {
 	for _, pattern := range windowsClientImagePatterns {
