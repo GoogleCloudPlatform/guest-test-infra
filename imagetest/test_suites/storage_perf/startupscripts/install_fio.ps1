efea02cc94dd7249c6303140560dfcd747fc6a4a
koln67 <koln@google.com>
2023-08-28 22:42:31 +0000

    commit files outside of imagetest

--- imagetest/test_suites/storage_perf/startupscripts/install_fio.ps1
+++
@@ -1,9 +0,0 @@
-cd C:\
-gsutil cp gs://gce-image-build-resources/windows/fio-3.35-x64.msi C:\
-try {
-	msiexec.exe "C:\fio-3.35-x64.msi" /quiet /norestart
-}
-catch {
-	Write-Output "msi file failed to resolve"
-	Write-Output $_
-}
