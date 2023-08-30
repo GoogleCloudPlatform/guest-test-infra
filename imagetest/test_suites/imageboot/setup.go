4ba6a7fe3d373b5b602c6406867e65f9d3145278
Amelia Crate <95060558+a-crate@users.noreply.github.com>
2023-08-30 21:53:07 +0000

    Expand secure boot test to cover supported images on ARM (#772)
    * Test secure boot on arm.
    
    Exclude distros that don't support SB on arm yet.
    
    * Stop skipping StartTime and BootTime tests when secureboot is unsupported
    
    * Remove debian 9 secureboot test exception, it's EOL and we don't publish images for it.
    
    * gofmt

--- imagetest/test_suites/imageboot/setup.go
+++ imagetest/test_suites/imageboot/setup.go
@@ -1,8 +1,8 @@
 package imageboot
 
 import (
+	"regexp"
 	"strconv"
-	"strings"
 	"time"
 
 	"github.com/GoogleCloudPlatform/guest-test-infra/imagetest"
@@ -10,6 +10,18 @@ import (
 
 // Name is the name of the test package. It must match the directory name.
 var Name = "imageboot"
+
+var sbUnsupported = []*regexp.Regexp{
+	// Permanent exceptions
+	regexp.MustCompile("debian-1[01].*arm64"),
+	regexp.MustCompile("windows-server-2012-r2-dc-core"), // Working but not easily testable and EOL in 1.5 months
+	// Temporary exceptions
+	regexp.MustCompile("debian-12.*arm64"),
+	// Waiting on MSFT signed shims:
+	regexp.MustCompile("rocky-linux-[89].*arm64"), // https://bugs.rockylinux.org/view.php?id=4027
+	regexp.MustCompile("rhel-9.*arm64"),           // https://bugzilla.redhat.com/show_bug.cgi?id=2103803
+	regexp.MustCompile("sles-15.*arm64"),          // https://bugzilla.suse.com/show_bug.cgi?id=1214761
+}
 
 // TestSetup sets up the test workflow.
 func TestSetup(t *imagetest.TestWorkflow) error {
@@ -28,22 +40,23 @@ func TestSetup(t *imagetest.TestWorkflow) error {
 	}
 	vm2.RunTests("TestGuestRebootOnHost")
 
-	if strings.Contains(t.Image, "debian-9") {
-		// secure boot is not supported on Debian 9
-		return nil
-	}
-
-	if strings.Contains(t.Image, "arm64") {
-		// secure boot is not supported on ARM images
-		return nil
-	}
-
 	vm3, err := t.CreateTestVM("vm3")
 	if err != nil {
 		return err
 	}
 	vm3.AddMetadata("start-time", strconv.Itoa(time.Now().Second()))
-	vm3.EnableSecureBoot()
-	vm3.RunTests("TestGuestSecureBoot|TestStartTime|TestBootTime")
+	vm3.RunTests("TestStartTime|TestBootTime")
+
+	for _, r := range sbUnsupported {
+		if r.MatchString(t.Image) {
+			return nil
+		}
+	}
+	vm4, err := t.CreateTestVM("vm4")
+	if err != nil {
+		return err
+	}
+	vm4.EnableSecureBoot()
+	vm4.RunTests("TestGuestSecureBoot")
 	return nil
 }
