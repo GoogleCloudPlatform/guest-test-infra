bcb30f23435d3204018ffd7bf858882b0d8d8d1d
koln67 <koln@google.com>
2023-08-31 20:39:19 +0000

    add windows curl/restmethod support

--- imagetest/test_suites/hotattach/hot_attach_test.go
+++ imagetest/test_suites/hotattach/hot_attach_test.go
@@ -5,12 +5,40 @@ package hotattach
 
 import (
 	"context"
+	"fmt"
 	"os/exec"
+	"runtime"
 	"strings"
 	"testing"
 
+	"github.com/GoogleCloudPlatform/guest-test-infra/imagetest/utils"
 	"google.golang.org/api/compute/v1"
 )
+
+func getProjectAndZone() (string, string, error) {
+	var projectZoneString string
+	if runtime.GOOS == "windows" {
+		procStatus, err := utils.RunPowershellCmd("Invoke-RestMethod -Headers @{'Metadata-Flavor' = 'Google'} -Uri \"http://metadata.google.internal/computeMetadata/v1/instance/zone\"")
+		if err != nil {
+			return "", "", fmt.Errorf("failed to get project or zone on windows: %v", err)
+		}
+		projectZoneString = strings.TrimSpace(procStatus.Stdout)
+	} else {
+		projectZoneBytes, err := exec.Command("curl", "http://metadata.google.internal/computeMetadata/v1/instance/zone", "-H", "Metadata-Flavor: Google").Output()
+		projectZoneString = strings.TrimSpace(string(projectZoneBytes))
+		if err != nil {
+			return "", "", fmt.Errorf("failed to get project or zone on linux: %v", err)
+		}
+	}
+	// projectZoneString should be in the fomrat projects/$PROJECTNUMBER/zone/$ZONE, and we want to pass in just the $ZONE value to detach the disk.
+	projectZoneSlice := strings.Split(string(projectZoneString), "/")
+	if strings.ToLower(projectZoneSlice[0]) != "projects" || strings.ToLower(projectZoneSlice[2]) != "zones" || len(projectZoneSlice) != 4 {
+		return "", "", fmt.Errorf("returned string for vm metata was the wrong format: got %s", projectZoneString)
+	}
+
+	// return format is (projectNumber, instanceZone, nil)
+	return projectZoneSlice[1], projectZoneSlice[3], nil
+}
 
 // TestEmptyAttach is a placeholder test for disk attaching.
 func TestEmptyAttach(t *testing.T) {
@@ -24,20 +52,10 @@ func TestEmptyAttach(t *testing.T) {
 	if err != nil {
 		t.Fatalf("failed to get hostname: %v %v", instName, err)
 	}
-	// fullzone looks like projects/$PROJECTNUMBER/zone/$ZONE, and we want to pass in just the $ZONE value to detach the disk.
-	projectZoneBytes, err := exec.Command("curl", "http://metadata.google.internal/computeMetadata/v1/instance/zone", "-H", "Metadata-Flavor: Google").Output()
-	projectZoneString := strings.TrimSpace(string(projectZoneBytes))
+	projectNumber, instanceZone, err := getProjectAndZone()
 	if err != nil {
-		t.Fatalf("failed to get project or zone: %v", err)
+		t.Fatalf("failed to get metadata for project or zone: %v", err)
 	}
-	projectZoneSlice := strings.Split(string(projectZoneString), "/")
-	if strings.ToLower(projectZoneSlice[0]) != "projects" || strings.ToLower(projectZoneSlice[2]) != "zones" || len(projectZoneSlice) != 4 {
-		t.Fatalf("returned string for vm metata was the wrong format: got %s", projectZoneString)
-	}
-
-	// prepare fields for the disk detach call
-	projectNumber := projectZoneSlice[1]
-	instanceZone := projectZoneSlice[3]
 	instancesDetachCall := instancesService.DetachDisk(projectNumber, instanceZone, strings.TrimSpace(string(instName)), diskName)
 	instancesDetachCall = instancesDetachCall.Context(ctx)
 	_, err = instancesDetachCall.Do()
