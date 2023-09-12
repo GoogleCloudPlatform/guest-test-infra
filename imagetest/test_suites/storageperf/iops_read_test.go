8904ff63529ffb8d1b15739c97060b37c76e6698
Anthony Lu <67125539+koln67@users.noreply.github.com>
2023-09-12 14:26:28 -0700

    Throughput testing (#781)

--- imagetest/test_suites/storageperf/iops_read_test.go
+++ imagetest/test_suites/storageperf/iops_read_test.go
@@ -16,13 +16,20 @@ import (
 )
 
 const (
-	commonFIOReadOptions = "--name=read_iops_test --filesize=10G --numjobs=8 --time_based --runtime=60s --ramp_time=2s --direct=1 --verify=0 --bs=4K --iodepth=256 --randrepeat=0 --rw=randread --group_reporting=1 --iodepth_batch_submit=256  --iodepth_batch_complete_max=256 --output-format=json"
+	commonFIORandReadOptions = "--name=read_iops_test --filesize=10G --numjobs=8 --time_based --runtime=60s --ramp_time=2s --direct=1 --verify=0 --bs=4K --iodepth=256 --randrepeat=0 --rw=randread --group_reporting=1 --iodepth_batch_submit=256  --iodepth_batch_complete_max=256 --output-format=json"
+	commonFIOSeqReadOptions  = "--name=read_bandwidth_test --filesize=10G --numjobs=4 --time_based --ramp_time=2s --runtime=1m --direct=1 --verify=0 --randrepeat=0 --thread --offset_increment=2G --bs=1M --iodepth=64 --rw=read --iodepth_batch_submit=64  --iodepth_batch_complete_max=64 --output-format=json"
 )
 
-func RunFIOReadWindows() ([]byte, error) {
+func RunFIOReadWindows(mode string) ([]byte, error) {
 	testdiskDrive := windowsDriveLetter + ":\\"
 	readIopsFile := "C:\\fio-read-iops.txt"
-	fioReadOptionsWindows := " -ArgumentList \"" + commonFIOReadOptions + " --output=" + readIopsFile + " --ioengine=windowsaio" + " --thread\"" + " -WorkingDirectory " + testdiskDrive + " -wait"
+	var readOptions string
+	if mode == sequentialMode {
+		readOptions = commonFIOSeqReadOptions
+	} else {
+		readOptions = commonFIORandReadOptions
+	}
+	fioReadOptionsWindows := " -ArgumentList \"" + readOptions + " --output=" + readIopsFile + " --ioengine=windowsaio" + " --thread\"" + " -WorkingDirectory " + testdiskDrive + " -wait"
 	// fioWindowsLocalPath is defined within storage_perf_utils.go
 	if procStatus, err := utils.RunPowershellCmd("Start-Process " + fioWindowsLocalPath + fioReadOptionsWindows); err != nil {
 		return []byte{}, fmt.Errorf("fio.exe returned with error: %v %s %s", err, procStatus.Stdout, procStatus.Stderr)
@@ -35,21 +42,33 @@ func RunFIOReadWindows() ([]byte, error) {
 	return []byte(readIopsJsonProcStatus.Stdout), nil
 }
 
-func RunFIOReadLinux() ([]byte, error) {
+func getLinuxSymlinkRead() (string, error) {
 	symlinkRealPath := ""
-	diskPartition, err := getMountDiskPartition(hyperdiskSize)
+	diskPartition, err := utils.GetMountDiskPartition(hyperdiskSize)
 	if err == nil {
 		symlinkRealPath = "/dev/" + diskPartition
 	} else {
 		errorString := err.Error()
-		symlinkRealPath, err = getMountDiskPartitionSymlink()
+		symlinkRealPath, err = utils.GetMountDiskPartitionSymlink(mountDiskName)
 		if err != nil {
 			errorString += err.Error()
-			return []byte{}, fmt.Errorf("failed to find symlink to mount disk with any method: errors %s", errorString)
+			return "", fmt.Errorf("failed to find symlink to mount disk with any method: errors %s", errorString)
 		}
 	}
-
-	fioReadOptionsLinuxSlice := strings.Fields(commonFIOReadOptions + " --filename=" + symlinkRealPath + " --ioengine=libaio")
+	return symlinkRealPath, nil
+}
+func RunFIOReadLinux(mode string) ([]byte, error) {
+	var readOptions string
+	if mode == sequentialMode {
+		readOptions = commonFIOSeqReadOptions
+	} else {
+		readOptions = commonFIORandReadOptions
+	}
+	symlinkRealPath, err := getLinuxSymlinkRead()
+	if err != nil {
+		return []byte{}, err
+	}
+	fioReadOptionsLinuxSlice := strings.Fields(readOptions + " --filename=" + symlinkRealPath + " --ioengine=libaio")
 	readIOPSJson, err := exec.Command("fio", fioReadOptionsLinuxSlice...).CombinedOutput()
 	if err != nil {
 		return []byte{}, fmt.Errorf("fio command failed with error: %v", err)
@@ -57,28 +76,58 @@ func RunFIOReadLinux() ([]byte, error) {
 	return readIOPSJson, nil
 }
 
-// TestReadIOPS checks that read IOPS are around the value listed in public docs.
-func TestReadIOPS(t *testing.T) {
-	var readIOPSJson []byte
+// TestRandomReadIOPS checks that random read IOPS are around the value listed in public docs.
+func TestRandomReadIOPS(t *testing.T) {
+	var randReadIOPSJson []byte
 	var err error
 	if runtime.GOOS == "windows" {
-		if readIOPSJson, err = RunFIOReadWindows(); err != nil {
-			t.Fatalf("windows fio read failed with error: %v", err)
+		if randReadIOPSJson, err = RunFIOReadWindows(randomMode); err != nil {
+			t.Fatalf("windows fio rand read failed with error: %v", err)
 		}
 	} else {
-		if readIOPSJson, err = RunFIOReadLinux(); err != nil {
-			t.Fatalf("linux fio read failed with error: %v", err)
+		if randReadIOPSJson, err = RunFIOReadLinux(randomMode); err != nil {
+			t.Fatalf("linux fio rand read failed with error: %v", err)
 		}
 	}
 
 	var fioOut FIOOutput
-	if err = json.Unmarshal(readIOPSJson, &fioOut); err != nil {
-		t.Fatalf("fio output %s could not be unmarshalled with error: %v", string(readIOPSJson), err)
+	if err = json.Unmarshal(randReadIOPSJson, &fioOut); err != nil {
+		t.Fatalf("fio output %s could not be unmarshalled with error: %v", string(randReadIOPSJson), err)
 	}
 
 	finalIOPSValue := fioOut.Jobs[0].ReadResult.IOPS
 	//TODO: Update this value to be equal to the input IOPS value, once it is implemented in this testing framework. For now, hyperdisk IOPS are the lesser of 100 IOPS per GiB of disk capacity or 350,000, if unspecified.
 	expectedHyperdiskIOPS := math.Min(100*hyperdiskSize, 350000)
+	if finalIOPSValue < iopsErrorMargin*expectedHyperdiskIOPS {
+		t.Fatalf("iops average was too low: expected close to %f, got  %f", expectedHyperdiskIOPS, finalIOPSValue)
+	}
+	t.Logf("iops test pass with %f iops, expected at least %f", finalIOPSValue, expectedHyperdiskIOPS)
+}
+
+// TestSequentialReadIOPS checks that sequential read IOPS are around the value listed in public docs.
+func TestSequentialReadIOPS(t *testing.T) {
+	var seqReadIOPSJson []byte
+	var err error
+	if runtime.GOOS == "windows" {
+		if seqReadIOPSJson, err = RunFIOReadWindows(sequentialMode); err != nil {
+			t.Fatalf("windows fio seq read failed with error: %v", err)
+		}
+	} else {
+		if seqReadIOPSJson, err = RunFIOReadLinux(sequentialMode); err != nil {
+			t.Fatalf("linux fio seq read failed with error: %v", err)
+		}
+	}
+
+	var fioOut FIOOutput
+	if err = json.Unmarshal(seqReadIOPSJson, &fioOut); err != nil {
+		t.Fatalf("fio output %s could not be unmarshalled with error: %v", string(seqReadIOPSJson), err)
+	}
+	var finalIOPSValue float64 = 0.0
+	for _, job := range fioOut.Jobs {
+		finalIOPSValue += job.ReadResult.IOPS
+	}
+	//TODO: Update this value to be equal to the input IOPS value, once it is implemented in this testing framework. For now, it is not clear what the sequential read iops value should be.
+	expectedHyperdiskIOPS := 5.0 * hyperdiskSize
 	if finalIOPSValue < iopsErrorMargin*expectedHyperdiskIOPS {
 		t.Fatalf("iops average was too low: expected close to %f, got  %f", expectedHyperdiskIOPS, finalIOPSValue)
 	}
