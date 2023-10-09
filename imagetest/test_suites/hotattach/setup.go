69e94f1821939480fbc6db4541d1048079728295
Amelia Crate <95060558+a-crate@users.noreply.github.com>
2023-10-09 23:33:19 +0000

    Handle arm images in hotattach suite (use an appropriate machine type, and skip until they're fixed) (#843)

--- imagetest/test_suites/hotattach/setup.go
+++ imagetest/test_suites/hotattach/setup.go
@@ -2,6 +2,7 @@ package hotattach
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/GoogleCloudPlatform/guest-test-infra/imagetest"
 	"google.golang.org/api/compute/v1"
@@ -29,7 +30,13 @@ func TestSetup(t *imagetest.TestWorkflow) error {
 		return fmt.Errorf("boot disk and mount disk must be different sizes for disk identification")
 	}
 	// The extra scope is required to call detachDisk and attachDisk.
-	hotattachParams := map[string]string{"machineType": "n2-standard-8", "extraScopes": "https://www.googleapis.com/auth/cloud-platform"}
+	hotattachParams := map[string]string{"extraScopes": "https://www.googleapis.com/auth/cloud-platform"}
+	if strings.Contains(t.Image, "arm64") {
+		t.Skip("t2a nvme udev rules aren't triggering correctly on reattach")
+		hotattachParams["machineType"] = "t2a-standard-8"
+	} else {
+		hotattachParams["machineType"] = "n2-standard-8"
+	}
 	vm, err := t.CreateTestVMMultipleDisks([]*compute.Disk{{Name: instanceName, Type: imagetest.PdBalanced, SizeGb: bootDiskSizeGB}, {Name: diskName, Type: imagetest.PdBalanced, SizeGb: mountDiskSizeGB}}, hotattachParams)
 	if err != nil {
 		return err
