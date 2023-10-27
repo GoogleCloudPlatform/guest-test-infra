dd0ce7dc2bf8af4c8c18a4c475b8191649b1387f
Amelia Crate <acrate@google.com>
2023-10-27 10:34:38 -0700

    Remove SAP workload test (and required scripts from concourse)

--- concourse/scripts/sap_post_script.sh
+++
@@ -1,6 +0,0 @@
-if grep -q "ERROR - Deployment Exited" /var/log/messages; then
-    echo "ERROR" > result.txt
-else
-    echo "SUCCESS" > result.txt
-fi
-gsutil cp result.txt gs://__BUCKET__/workload-tests/sap/__RUNID__/run_result
