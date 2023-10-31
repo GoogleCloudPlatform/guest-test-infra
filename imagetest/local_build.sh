00eff00bb8bc966cdeca51c7db9cdb2410b90563
koln67 <koln@google.com>
2023-10-31 23:58:25 +0000

    add comments to shell build script

--- imagetest/local_build.sh
+++ imagetest/local_build.sh
@@ -1,3 +1,5 @@
+# rootpath should be an absolute path where you want test output files storedm, such as /home/username
+# suite is the name of the test suite
 rootpath=$1
 suite=$2
 go build -o $rootpath/wrapper.amd64 ./cmd/wrapper/main.go
