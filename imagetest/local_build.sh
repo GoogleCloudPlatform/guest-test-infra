64d2b647b94e0bfded71f2c481bdc046e320d300
koln67 <koln@google.com>
2023-11-01 22:14:04 +0000

    Add local build options

--- imagetest/local_build.sh
+++ imagetest/local_build.sh
@@ -1,11 +1,47 @@
-# rootpath should be an absolute path where you want test output files storedm, such as /home/username
-# suite is the name of the test suite
-rootpath=$1
-suite=$2
-go build -o $rootpath/wrapper.amd64 ./cmd/wrapper/main.go
-go build -o $rootpath/manager ./cmd/manager/main.go
+# usage: local_build.sh -o $outspath -c $cmdroot -s $suites_to_build
+# the output path of the test files
+outpath=.
+# the root of the cmd folder, default assumes this script is run from imagetest
+cmdroot=.
+# the suites to build, space separated. all suites are built by default
+suites=*
+
+while getopts "o:c:s:" arg; do
+  case $arg in 
+    o) outpath=$OPTARG;;
+    c) cmdroot=$OPTARG;;
+    s) suites=$OPTARG;;
+    *) echo "unknown arg"
+  esac
+done 
+echo "outspath is $outpath"
+echo "cmdroot is $cmdroot"
+echo "suites are $suites"
+go mod download
+echo "1"
+go build -o $outpath/wrapper.amd64 $cmdroot/cmd/wrapper/main.go
+GOARCH=arm64 go build -o $outpath/wrapper.arm64 $cmdroot/cmd/wrapper/main.go
+GOOS=windows GOARCH=amd64 go build -o $outpath/wrapp64.exe $cmdroot/cmd/wrapper/main.go
+GOOS=windows GOARCH=386 go build -o $outpath/wrapp32.exe $cmdroot/cmd/wrapper/main.go
+echo "2"
+go build -o $outpath/manager $cmdroot/cmd/manager/main.go
 cd test_suites
-cd $suite
-go test -c -tags cit
-./"${suite}.test" -test.list '.*' > $rootpath/"${suite}_tests.txt"
-mv "${suite}.test" $rootpath/"${suite}.amd64.test"
+echo "3"
+for suite in $suites; do
+  [[ -d $suite ]] || continue
+  cd $suite
+  echo "4"
+  go test -c -tags cit
+  ./"${suite}.test" -test.list '.*' > $outpath/"${suite}_tests.txt"
+  mv "${suite}.test" $outpath/"${suite}.amd64.test"
+  GOARCH=arm64 go test -c -tags cit
+  mv "${suite}.test" "$outpath/${suite}.arm64.test"
+  echo "5"
+  GOOS=windows GOARCH=amd64 go test -c -tags cit
+  echo "6"
+  if [ -f "${suite}.test.exe" ]; then mv "${suite}.test.exe" "$outpath/${suite}64.exe"; fi;
+  echo "7"
+  GOOS=windows GOARCH=386 go test -c -tags cit
+  if [ -f "${suite}.test.exe" ]; then mv "${suite}.test.exe" "$outpath/${suite}32.exe"; fi;
+  cd ..
+done
