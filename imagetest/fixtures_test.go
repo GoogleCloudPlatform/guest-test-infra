0b27852c700b2a1cf683534d54f86625a4e9ca3b
Amelia Crate <95060558+a-crate@users.noreply.github.com>
2023-11-02 20:27:32 +0000

    Add the ability for TestVM to run on an instance from the beta API. (#896)
    * Add the ability for TestVM to run on an instance from the beta API.
    
    The abstraction of setting one of instance or instancebeta is not super elegant but it matches the abstractions present in daisy. There is no unified way to represent a compute instance which is either GA or beta. Generics are not an option because we can't directly reference arbitrary properties on instiated generic types, something we need to do frequently.
    
    Added support for beta instances in existing TestVM methods. Expanded unit tests for TestVM methods to cover instancebeta cases.
    I modified the behavior in a couple TestVM methods which were clobbering ShieldedInstanceConfig, it's now possible to enable both secure boot and shielded vm in the same instance without them clobbering each other.
    
    Added a function to create a TestVM from a daisy.InstanceBeta and a slice of compute (GA) disks. Added unit tests for this.
    
    * gofmt

--- imagetest/fixtures_test.go
+++ imagetest/fixtures_test.go
@@ -27,6 +27,21 @@ func TestAddMetadata(t *testing.T) {
 	if val, ok := tvm.instance.Metadata["key"]; !ok || val != "val2" {
 		t.Errorf("invalid metadata set")
 	}
+	tvmb, err := twf.CreateTestVMBeta("vmBeta")
+	if err != nil {
+		t.Errorf("failed to create test vm: %v", err)
+	}
+	tvmb.AddMetadata("key", "val")
+	if tvmb.instancebeta.Metadata == nil {
+		t.Errorf("failed to set VM metadata")
+	}
+	if val, ok := tvmb.instancebeta.Metadata["key"]; !ok || val != "val" {
+		t.Errorf("invalid metadata set")
+	}
+	tvmb.AddMetadata("key", "val2")
+	if val, ok := tvmb.instancebeta.Metadata["key"]; !ok || val != "val2" {
+		t.Errorf("invalid metadata set")
+	}
 }
 
 // TestReboot tests that *TestVM.Reboot succeeds and that the appropriate stop
@@ -55,6 +70,87 @@ func TestReboot(t *testing.T) {
 	}
 	if lastStep.WaitForInstancesSignal == nil {
 		t.Error("not wait step")
+	}
+	if step, ok := twf.wf.Steps["wait-started-vm-1"]; !ok || step != lastStep {
+		t.Error("not wait-started-vm-1 step")
+	}
+}
+
+func TestCreateVMFromInstanceBeta(t *testing.T) {
+	twf := NewTestWorkflowForUnitTest("name", "image", "30m")
+	disks := []*compute.Disk{{Name: "vm"}, {Name: "mountdisk", Type: PdSsd, SizeGb: 100}}
+	inst := &daisy.InstanceBeta{}
+	inst.Name = "vm"
+	tvm, err := twf.CreateTestVMFromInstanceBeta(inst, disks)
+	if err != nil {
+		t.Errorf("failed to create test vm: %v", err)
+	}
+	// once found, expect createInstancesStep.CreateInstances != nil
+	// once found, expect createDisksStep.CreateDisks != nil
+	var createInstancesStep, createDisksStep *daisy.Step
+	for _, step := range twf.wf.Steps {
+		// there should only be one create instance step
+		if step.CreateInstances != nil {
+			if createInstancesStep == nil {
+				createInstancesStep = step
+			} else {
+				t.Errorf("workflow has multiple create instance steps when it should not")
+			}
+		}
+
+		if step.CreateDisks != nil {
+			if createDisksStep == nil {
+				createDisksStep = step
+			} else {
+				t.Errorf("workflow has multiple create disk steps when it should not")
+			}
+		}
+	}
+
+	if createInstancesStep == nil || createInstancesStep.CreateInstances == nil {
+		t.Errorf("failed to find create instances step when creating multiple disks")
+	}
+
+	if createDisksStep == nil || createDisksStep.CreateDisks == nil {
+		t.Errorf("failed to find create disks step when creating multiple disks")
+	}
+
+	daisyStepDisksSlice := *(createDisksStep.CreateDisks)
+	if len(disks) != len(daisyStepDisksSlice) {
+		t.Errorf("found incorrect number of disks in create disk step: expected %d, got %d",
+			len(disks), len(daisyStepDisksSlice))
+	}
+
+	if twf.counter != 0 {
+		t.Errorf("step counter not starting at 0")
+	}
+	if err := tvm.Reboot(); err != nil {
+		t.Errorf("failed to reboot: %v", err)
+	}
+	if twf.counter != 1 {
+		t.Errorf("step counter not incremented")
+	}
+	if _, ok := twf.wf.Steps["stop-vm-1"]; !ok {
+		t.Errorf("wait-vm-1 step missing")
+	}
+	lastStep, err := twf.getLastStepForVM("vm")
+	if err != nil {
+		t.Errorf("failed to get last step for vm: %v", err)
+	}
+	if lastStep.WaitForInstancesSignal == nil {
+		t.Error("not wait step")
+	}
+	waitForInstancesSignalSlice := (*lastStep.WaitForInstancesSignal)
+	if len(waitForInstancesSignalSlice) == 0 {
+		t.Error("waitForInstancesSignal has no elements in slice")
+	}
+	waitGuestAttribute := waitForInstancesSignalSlice[0].GuestAttribute
+	if waitGuestAttribute == nil {
+		t.Error("could not find guest attribute wait step")
+	}
+	gaNameSpace, gaKeyName := waitGuestAttribute.Namespace, waitGuestAttribute.KeyName
+	if gaNameSpace != utils.GuestAttributeTestNamespace || gaKeyName != utils.GuestAttributeTestKey {
+		t.Errorf("wrong guest attribute: got namespace, keyname as %s, %s but expected %s, %s", gaNameSpace, gaKeyName, utils.GuestAttributeTestNamespace, utils.GuestAttributeTestKey)
 	}
 	if step, ok := twf.wf.Steps["wait-started-vm-1"]; !ok || step != lastStep {
 		t.Error("not wait-started-vm-1 step")
@@ -310,6 +406,26 @@ func TestResizeDiskAndReboot(t *testing.T) {
 	if twf.wf.Steps["wait-started-vm-2"] != step {
 		t.Error("not wait-started-vm-2 step")
 	}
+	tvmb, err := twf.CreateTestVM("vmbeta")
+	if err != nil {
+		t.Errorf("failed to create test vm: %v", err)
+	}
+	if err := tvmb.ResizeDiskAndReboot(200); err != nil {
+		t.Errorf("failed to reboot: %v", err)
+	}
+	if _, ok := twf.wf.Steps["resize-disk-vm-1"]; !ok {
+		t.Errorf("wait-vm-1 step missing")
+	}
+	step, err = twf.getLastStepForVM("vmbeta")
+	if err != nil {
+		t.Errorf("failed to get last step for vm: %v", err)
+	}
+	if step.WaitForInstancesSignal == nil {
+		t.Error("not wait step")
+	}
+	if twf.wf.Steps["wait-started-vmbeta-4"] != step {
+		t.Error("not wait-started-vmbeta-4")
+	}
 }
 
 // TestEnableSecureBoot tests that *TestVM.EnableSecureBoot succeeds and
@@ -320,12 +436,17 @@ func TestEnableSecureBoot(t *testing.T) {
 	if err != nil {
 		t.Errorf("failed to create test vm: %v", err)
 	}
-	if tvm.instance.ShieldedInstanceConfig != nil {
-		t.Errorf("VM didn't have nil SIC at creation")
-	}
 	tvm.EnableSecureBoot()
-	if tvm.instance.ShieldedInstanceConfig == nil {
-		t.Errorf("VM SIC is nil")
+	if !tvm.instance.ShieldedInstanceConfig.EnableSecureBoot {
+		t.Errorf("test vm does not have secure boot enabled")
+	}
+	tvmb, err := twf.CreateTestVMBeta("vmbeta")
+	if err != nil {
+		t.Errorf("failed to create test vm: %v", err)
+	}
+	tvmb.EnableSecureBoot()
+	if !tvmb.instancebeta.ShieldedInstanceConfig.EnableSecureBoot {
+		t.Errorf("test vmbeta does not have secure boot enabled")
 	}
 }
 
@@ -404,6 +525,17 @@ func TestUseGVNIC(t *testing.T) {
 	if tvm.instance.NetworkInterfaces[0].NicType != "GVNIC" {
 		t.Errorf("VM Network Interface type not set to GVNIC")
 	}
+	tvmb, err := twf.CreateTestVMBeta("vmbeta")
+	if err != nil {
+		t.Errorf("failed to create test vm: %v", err)
+	}
+	tvmb.UseGVNIC()
+	if tvmb.instancebeta.NetworkInterfaces == nil {
+		t.Errorf("VM Network Interfaces is nil")
+	}
+	if tvmb.instancebeta.NetworkInterfaces[0].NicType != "GVNIC" {
+		t.Errorf("VM Network Interface type not set to GVNIC")
+	}
 }
 
 // TestAddAliasIPRanges tests that *TestVM.AddAliasIPRanges succeeds and that
@@ -430,6 +562,22 @@ func TestAddAliasIPRanges(t *testing.T) {
 	if tvm.instance.NetworkInterfaces[0].AliasIpRanges == nil {
 		t.Errorf("VM alias IP is nil")
 	}
+	tvmb, err := twf.CreateTestVMBeta("vmbeta")
+	if err != nil {
+		t.Errorf("failed to create test vm: %v", err)
+	}
+	if err := tvmb.AddAliasIPRanges("aliasIPRange", "rangeName"); err == nil {
+		t.Fatalf("shouldn't be able to set alias IP without calling setcustomnetwork")
+	}
+	if err := tvmb.AddCustomNetwork(network, nil); err != nil {
+		t.Errorf("failed to set custom network: %v", err)
+	}
+	if err := tvmb.AddAliasIPRanges("aliasIPRange", "rangeName"); err != nil {
+		t.Fatalf("error adding alias IP range: %v", err)
+	}
+	if tvmb.instancebeta.NetworkInterfaces[0].AliasIpRanges == nil {
+		t.Errorf("VM alias IP is nil")
+	}
 }
 
 // TestSetCustomNetwork tests that *TestVM.AddCustomNetwork succeeds and that
@@ -445,6 +593,17 @@ func TestSetCustomNetwork(t *testing.T) {
 		t.Errorf("failed to create network: %v", err)
 	}
 	if err := tvm.AddCustomNetwork(network, nil); err != nil {
+		t.Errorf("failed to set custom network: %v", err)
+	}
+	tvmb, err := twf.CreateTestVMBeta("vmbeta")
+	if err != nil {
+		t.Errorf("failed to create test vm: %v", err)
+	}
+	network, err = twf.CreateNetwork("network", true)
+	if err != nil {
+		t.Errorf("failed to create network: %v", err)
+	}
+	if err := tvmb.AddCustomNetwork(network, nil); err != nil {
 		t.Errorf("failed to set custom network: %v", err)
 	}
 }
@@ -574,6 +733,25 @@ func TestAddUser(t *testing.T) {
 	if keys, ok := tvm.instance.Metadata["ssh-keys"]; !ok || keys != "username:PUBKEY1\nusername2:PUBKEY2" {
 		t.Errorf("\"ssh-keys\" key malformed after repeated entry")
 	}
+	tvmb, err := twf.CreateTestVMBeta("vmBeta")
+	if err != nil {
+		t.Errorf("failed to create network: %v", err)
+	}
+	tvmb.AddUser("username", "PUBKEY1")
+	if tvmb.instancebeta.Metadata == nil {
+		t.Fatalf("instance metadata is nil")
+	}
+	keys, ok = tvmb.instancebeta.Metadata["ssh-keys"]
+	if !ok {
+		t.Fatalf("\"ssh-keys\" key not added to instance")
+	}
+	if keys != "username:PUBKEY1" {
+		t.Fatalf("\"ssh-keys\" key malformed")
+	}
+	tvmb.AddUser("username2", "PUBKEY2")
+	if keys, ok := tvmb.instancebeta.Metadata["ssh-keys"]; !ok || keys != "username:PUBKEY1\nusername2:PUBKEY2" {
+		t.Errorf("\"ssh-keys\" key malformed after repeated entry")
+	}
 }
 
 func TestForceMachineType(t *testing.T) {
@@ -602,6 +780,17 @@ func TestForceZone(t *testing.T) {
 	}
 	tvm.ForceZone("us-east1-a")
 	if tvm.instance.Zone != "us-east1-a" {
+		t.Errorf("could not set test zone, got %q, want us-east1-a", tvm.instance.Zone)
+	}
+	tvmb, err := twf.CreateTestVMBeta("vmbeta")
+	if err != nil {
+		t.Errorf("failed to create test vm: %v", err)
+	}
+	if tvmb.instancebeta.Zone != "" {
+		t.Errorf("machine zone already set: %v", tvmb.instancebeta.Zone)
+	}
+	tvmb.ForceZone("us-east1-a")
+	if tvmb.instancebeta.Zone != "us-east1-a" {
 		t.Errorf("could not set test zone, got %q, want us-east1-a", tvm.instance.Zone)
 	}
 }
