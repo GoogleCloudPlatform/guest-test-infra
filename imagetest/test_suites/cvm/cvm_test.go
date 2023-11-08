68d44d83336fd5642304a42c7ec7c97128d36f2c
Amelia Crate <95060558+a-crate@users.noreply.github.com>
2023-11-08 18:08:09 +0000

    Revert "Add SEV_SNP and TDX enablement tests (#905)" (#907)
    This reverts commit fab96ec82e74e0431646047b9b9a961d0f7c219b.

--- imagetest/test_suites/cvm/cvm_test.go
+++ imagetest/test_suites/cvm/cvm_test.go
@@ -7,10 +7,8 @@ import (
 )
 
 var sevMsgList = []string{"AMD Secure Encrypted Virtualization (SEV) active", "AMD Memory Encryption Features active: SEV", "Memory Encryption Features active: AMD SEV"}
-var sevSnpMsgList = []string{"AMD Secure Encrypted Virtualization (SEV) active", "SEV: SNP guest platform device intitialized", "Memory Encryption Features active: AMD SEV SEV-ES SEV-SNP"}
-var tdxMsgList = []string{"Memory Encryption Features active: TDX", "Memory Encryption Features active: Intel TDX"}
 
-func TestSEVEnabled(t *testing.T) {
+func TestCVMEnabled(t *testing.T) {
 	output, err := exec.Command("/bin/sh", "-c", "sudo dmesg | grep SEV").Output()
 	if err != nil {
 		t.Fatalf("Error: %v", err)
@@ -21,30 +19,4 @@ func TestSEVEnabled(t *testing.T) {
 		}
 	}
 	t.Fatal("Error: SEV not active or found")
-}
-
-func TestSEVSNPEnabled(t *testing.T) {
-	output, err := exec.Command("/bin/sh", "-c", "sudo dmesg | grep SEV").Output()
-	if err != nil {
-		t.Fatalf("Error: %v", err)
-	}
-	for _, msg := range sevSnpMsgList {
-		if strings.Contains(string(output), msg) {
-			return
-		}
-	}
-	t.Fatal("Error: SEV not active or found")
-}
-
-func TestTDXEnabled(t *testing.T) {
-	output, err := exec.Command("/bin/sh", "-c", "sudo dmesg | grep TDX").Output()
-	if err != nil {
-		t.Fatalf("Error: %v", err)
-	}
-	for _, msg := range tdxMsgList {
-		if strings.Contains(string(output), msg) {
-			return
-		}
-	}
-	t.Fatal("Error: TDX not active or found")
 }
