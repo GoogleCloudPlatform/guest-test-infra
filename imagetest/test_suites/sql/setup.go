bbeb7de5c819b743dc5b99ccec78529e93476255
Amelia Crate <95060558+a-crate@users.noreply.github.com>
2024-01-10 12:04:07 -0800

    Don't create windowscontainers or sql VMs if the image being tested isn't windows (#1012)

--- imagetest/test_suites/sql/setup.go
+++ imagetest/test_suites/sql/setup.go
@@ -4,6 +4,7 @@ import (
 	"embed"
 
 	"github.com/GoogleCloudPlatform/guest-test-infra/imagetest"
+	"github.com/GoogleCloudPlatform/guest-test-infra/imagetest/utils"
 )
 
 // Name is the name of the test package. It must match the directory name.
@@ -28,67 +29,68 @@ const (
 
 // TestSetup sets up the test workflow.
 func TestSetup(t *imagetest.TestWorkflow) error {
-	defaultNetwork, err := t.CreateNetwork("default-network", false)
-	if err != nil {
-		return err
-	}
-	defaultSubnetwork, err := defaultNetwork.CreateSubnetwork("default-subnetwork", "192.168.0.0/24")
-	if err != nil {
-		return err
-	}
-	if err := defaultNetwork.CreateFirewallRule("allow-sql-tcp", "tcp", []string{"135", "1433", "1434", "4022", "5022"}, []string{"192.168.0.0/24"}); err != nil {
-		return err
-	}
-	if err := defaultNetwork.CreateFirewallRule("allow-sql-udp", "udp", []string{"1434"}, []string{"192.168.0.0/24"}); err != nil {
-		return err
-	}
+	if utils.HasFeature(t.Image, "WINDOWS") {
+		defaultNetwork, err := t.CreateNetwork("default-network", false)
+		if err != nil {
+			return err
+		}
+		defaultSubnetwork, err := defaultNetwork.CreateSubnetwork("default-subnetwork", "192.168.0.0/24")
+		if err != nil {
+			return err
+		}
+		if err := defaultNetwork.CreateFirewallRule("allow-sql-tcp", "tcp", []string{"135", "1433", "1434", "4022", "5022"}, []string{"192.168.0.0/24"}); err != nil {
+			return err
+		}
+		if err := defaultNetwork.CreateFirewallRule("allow-sql-udp", "udp", []string{"1434"}, []string{"192.168.0.0/24"}); err != nil {
+			return err
+		}
 
-	// Get the startup scripts as byte arrays.
-	serverStartupByteArr, err := scripts.ReadFile(serverStartupScriptURL)
-	if err != nil {
-		return err
-	}
-	clientStartupByteArr, err := scripts.ReadFile(clientStartupScriptURL)
-	if err != nil {
-		return err
-	}
-	serverStartup := string(serverStartupByteArr)
-	clientStartup := string(clientStartupByteArr)
+		// Get the startup scripts as byte arrays.
+		serverStartupByteArr, err := scripts.ReadFile(serverStartupScriptURL)
+		if err != nil {
+			return err
+		}
+		clientStartupByteArr, err := scripts.ReadFile(clientStartupScriptURL)
+		if err != nil {
+			return err
+		}
+		serverStartup := string(serverStartupByteArr)
+		clientStartup := string(clientStartupByteArr)
 
-	serverVM, err := t.CreateTestVM(serverConfig.name)
-	if err != nil {
-		return err
-	}
-	if err := serverVM.AddCustomNetwork(defaultNetwork, defaultSubnetwork); err != nil {
-		return err
-	}
-	if err := serverVM.SetPrivateIP(defaultNetwork, serverConfig.ip); err != nil {
-		return err
-	}
+		serverVM, err := t.CreateTestVM(serverConfig.name)
+		if err != nil {
+			return err
+		}
+		if err := serverVM.AddCustomNetwork(defaultNetwork, defaultSubnetwork); err != nil {
+			return err
+		}
+		if err := serverVM.SetPrivateIP(defaultNetwork, serverConfig.ip); err != nil {
+			return err
+		}
 
-	clientVM, err := t.CreateTestVM(clientConfig.name)
-	if err != nil {
-		return err
-	}
-	if err := clientVM.AddCustomNetwork(defaultNetwork, defaultSubnetwork); err != nil {
-		return err
-	}
-	if err := clientVM.SetPrivateIP(defaultNetwork, clientConfig.ip); err != nil {
-		return err
-	}
-	clientVM.AddMetadata("enable-guest-attributes", "TRUE")
-	clientVM.AddMetadata("sqltarget", serverConfig.ip)
+		clientVM, err := t.CreateTestVM(clientConfig.name)
+		if err != nil {
+			return err
+		}
+		if err := clientVM.AddCustomNetwork(defaultNetwork, defaultSubnetwork); err != nil {
+			return err
+		}
+		if err := clientVM.SetPrivateIP(defaultNetwork, clientConfig.ip); err != nil {
+			return err
+		}
+		clientVM.AddMetadata("enable-guest-attributes", "TRUE")
+		clientVM.AddMetadata("sqltarget", serverConfig.ip)
 
-	serverVM.AddMetadata("windows-startup-script-ps1", serverStartup)
-	clientVM.AddMetadata("windows-startup-script-ps1", clientStartup)
+		serverVM.AddMetadata("windows-startup-script-ps1", serverStartup)
+		clientVM.AddMetadata("windows-startup-script-ps1", clientStartup)
 
-	vm1, err := t.CreateTestVM("vm1")
-	if err != nil {
-		return err
+		vm1, err := t.CreateTestVM("vm1")
+		if err != nil {
+			return err
+		}
+		vm1.RunTests("TestSqlVersion|TestPowerPlan")
+		clientVM.RunTests("TestRemoteConnectivity")
+		serverVM.RunTests("TestPowerPlan")
 	}
-	vm1.RunTests("TestSqlVersion|TestPowerPlan")
-	clientVM.RunTests("TestRemoteConnectivity")
-	serverVM.RunTests("TestPowerPlan")
-
 	return nil
 }
--- imagetest/test_suites/windowscontainers/setup.go
+++ imagetest/test_suites/windowscontainers/setup.go
@@ -2,6 +2,7 @@ package windowscontainers
 
 import (
 	"github.com/GoogleCloudPlatform/guest-test-infra/imagetest"
+	"github.com/GoogleCloudPlatform/guest-test-infra/imagetest/utils"
 )
 
 // Name is the name of the test package. It must match the directory name.
@@ -9,9 +10,11 @@ var Name = "windowscontainers"
 
 // TestSetup sets up the test workflow.
 func TestSetup(t *imagetest.TestWorkflow) error {
-	_, err := t.CreateTestVM("vm")
-	if err != nil {
-		return err
+	if utils.HasFeature(t.Image, "WINDOWS") {
+		_, err := t.CreateTestVM("vm")
+		if err != nil {
+			return err
+		}
 	}
 
 	return nil
