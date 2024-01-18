1e28515f55d19a803e482d61a16b3ba7868ac320
Amelia Crate <acrate@google.com>
2024-01-17 16:57:43 -0800

    Implement TestSockets in windows

--- imagetest/test_suites/security/image_security_test.go
+++ imagetest/test_suites/security/image_security_test.go
@@ -382,13 +382,15 @@ var (
 
 // TestSockets tests that only whitelisted ports are listening globally.
 func TestSockets(t *testing.T) {
-	utils.LinuxOnly(t)
-	// TODO Windows support
+	if utils.IsWindows() {
+		validateSocketsWindows(t)
+		return
+	}
 	// print listening TCP or UDP sockets with no header and no name
 	// resolution.
 	out, err := exec.Command("ss", "-Hltun").Output()
 	if err != nil && err.Error() == "exit status 255" {
-		// Probably on an OS with a versio of ss too old to support -H
+		// Probably on an OS with a version of ss too old to support -H
 		out, err = exec.Command("ss", "-ltun").Output()
 		_, a, _ := strings.Cut(string(out), "\n")
 		out = []byte(a)
@@ -474,6 +476,97 @@ func validateSockets(listening, allowed []string) error {
 	}
 
 	return nil
+}
+
+func validateSocketsWindows(t *testing.T) {
+	t.Helper()
+	portRe := regexp.MustCompile("[0-9]+\r?\n")
+	UDPListeners, err := utils.RunPowershellCmd(`Get-NetUDPEndpoint | Format-List -Property LocalPort`)
+	if err != nil {
+		t.Fatalf("could not get udp listeners: %v", err)
+	}
+	for _, port := range portRe.FindAllString(UDPListeners.Stdout, -1) {
+		port = strings.TrimSuffix(strings.TrimSuffix(port, "\n"), "\r")
+		p, err := strconv.Atoi(port)
+		if err != nil {
+			continue
+		}
+		switch p {
+		case 123: // NTP
+			continue
+		case 137: // NetBIOS name service
+			continue
+		case 138: // NetBIOS
+			continue
+		case 500: // IPSec IKE
+			continue
+		case 546: // dhcp
+			continue
+		case 3389: // RDP
+			continue
+		case 3544: // Teredo
+			continue
+		case 4500: // IPSec NAT Traversal
+			continue
+		case 5353: // multicast dns
+			continue
+		case 5355: // LLMNR
+			continue
+		case 5050: // svchost
+			continue
+		}
+		owningprocess, err := utils.RunPowershellCmd(fmt.Sprintf(`(Get-Process -Id "$((Get-NetUDPEndpoint -LocalPort %d).OwningProcess)").ProcessName`, p))
+		if err != nil {
+			t.Errorf("could not find process name listening on port %d", p)
+		}
+		pname := strings.TrimSpace(owningprocess.Stdout)
+		if p > 49152 && pname == "svchost" {
+			continue
+		}
+		t.Errorf("found udp listener on unexpected port %d (process name %s)", p, pname)
+	}
+	TCPListeners, err := utils.RunPowershellCmd(`Get-NetTCPConnection -State Listen | Format-List -Property LocalPort`)
+	if err != nil {
+		t.Fatalf("could not get tcp listeners: %v", err)
+	}
+	for _, port := range portRe.FindAllString(TCPListeners.Stdout, -1) {
+		port = strings.TrimSuffix(strings.TrimSuffix(port, "\n"), "\r")
+		p, err := strconv.Atoi(port)
+		if err != nil {
+			continue
+		}
+		switch p {
+		case 22: // sshd
+			continue
+		case 135: // msrpc
+			continue
+		case 139: // NetBIOS
+			continue
+		case 445: // microsoft-ds
+			continue
+		case 3389: // rdp
+			continue
+		case 5985: // winrm
+			continue
+		case 5986: // winrm
+			continue
+		case 20201: // ops agent
+			continue
+		case 20202: // ops agent
+			continue
+		case 47001: // windows remote management
+			continue
+		}
+		owningprocess, err := utils.RunPowershellCmd(fmt.Sprintf(`(Get-Process -Id "$((Get-TCPConnection -State Listen -LocalPort %d).OwningProcess)").ProcessName`, p))
+		if err != nil {
+			t.Errorf("could not find process name listening on port %d", p)
+		}
+		pname := strings.TrimSpace(owningprocess.Stdout)
+		if p > 49152 && (pname == "svchost" || pname == "Idle") {
+			continue
+		}
+		t.Errorf("found tcp listener on unexpected port %d (process %s)", p, pname)
+	}
 }
 
 func isInSlice(entry string, list []string) bool {
