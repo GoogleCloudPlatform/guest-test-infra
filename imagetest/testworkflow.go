38cbaa6493d6fadd3addc219f1255663cfa7b41b
Amelia Crate <95060558+a-crate@users.noreply.github.com>
2024-01-26 14:29:15 -0800

    Switch disk types to hyperdisk extreme for machine types that don't support PD (#1032)
    * Switch disk types to hyperdisk extreme for machine types that don't support PD
    
    * only modify disks using the default type when a machinetype doesn't support PD

--- imagetest/testworkflow.go
+++ imagetest/testworkflow.go
@@ -491,6 +491,7 @@ func finalizeWorkflows(ctx context.Context, tests []*TestWorkflow, zone, gcsPref
 			arch = "arm64"
 		}
 
+		createDisksStep, createDisksOk := twf.wf.Steps[createDisksStepName]
 		createVMsStep, ok := twf.wf.Steps[createVMsStepName]
 		if ok {
 			for _, vm := range createVMsStep.CreateInstances.Instances {
@@ -501,6 +502,15 @@ func finalizeWorkflows(ctx context.Context, tests []*TestWorkflow, zone, gcsPref
 				}
 				if vm.Zone != "" && vm.Zone != twf.wf.Zone {
 					log.Printf("VM %s zone is set to %s, differing from workflow zone %s for test %s, not overriding\n", vm.Name, vm.Zone, twf.wf.Zone, twf.Name)
+				}
+				if createDisksOk && (strings.HasPrefix(vm.MachineType, "c4-") || strings.HasPrefix(vm.MachineType, "n4-")) {
+					for _, attachedDisk := range vm.Disks {
+						for _, disk := range *createDisksStep.CreateDisks {
+							if attachedDisk.Source == disk.Name && disk.Type == "" {
+								disk.Type = HyperdiskBalanced
+							}
+						}
+					}
 				}
 			}
 		}
