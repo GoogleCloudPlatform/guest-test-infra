e142ef62c529c0542db1235570b480f9c2507259
Amelia Crate <acrate@google.com>
2024-02-07 11:21:30 -0800

    Refactor licensevalidation suite. Instead of checking a hard-coded list of license codes, generate a list of expected licenses based on what we know about the image and compare that to the license urls on it. Then check that all license codes present on the image propogate to the VM created from it.
    While I was messing with these suite I internationalized the ActivationCheck per the FR for that

--- imagetest/test_suites/licensevalidation/license_test.go
+++ imagetest/test_suites/licensevalidation/license_test.go
@@ -4,6 +4,7 @@
 package licensevalidation
 
 import (
+	"sort"
 	"strings"
 	"testing"
 
@@ -19,14 +20,68 @@ func TestWindowsActivationStatus(t *testing.T) {
 		t.Skip("Activation status only checked on server images.")
 	}
 
-	command := "cscript C:\\Windows\\system32\\slmgr.vbs /dli"
-	output, err := utils.RunPowershellCmd(command)
+    activationStatus, err := utils.RunPowershellCmd(`(Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SoftwareProtectionPlatform\Activation').ProductActivationResult`)
+    if err != nil {
+	    t.Fatalf("could not check activation status: %v", err)
+    }
+    if strings.TrimSpace(activationStatus.Stdout) != "0" {
+	    t.Errorf("unexpected activation status, got %s want 0", strings.TrimSpace(activationStatus.Stdout))
+    }
+}
+
+func TestLicenses(t *testing.T) {
+	ctx := utils.Context(t)
+	elicensecodes, err := utils.GetMetadata(ctx, "instance", "attributes", "expected-license-codes")
 	if err != nil {
-		t.Fatalf("Error getting license status: %v", err)
+		t.Fatalf("Failed to get expected licenses: %v", err)
+	}
+	expectedLicenseCodes := strings.Split(elicensecodes, ",")
+	var actualLicenseCodes []string
+	licenseNums, err := utils.GetMetadata(ctx, "instance", "licenses")
+	if err != nil {
+		t.Fatalf("could not get instance licenses: %v", err)
+	}
+	for _, lnum := range strings.Split(licenseNums, "\n") {
+		lnum = strings.TrimSpace(lnum)
+		if lnum == "" {
+			continue
+		}
+		id, err := utils.GetMetadata(ctx, "instance", "licenses", lnum, "id")
+		if err != nil {
+			t.Fatalf("could not get license %s id: %v", lnum, err)
+		}
+		actualLicenseCodes = append(actualLicenseCodes, id)
+	}
+	elicenses, err := utils.GetMetadata(ctx, "instance", "attributes", "expected-licenses")
+	if err != nil {
+		t.Fatalf("Failed to get expected licenses: %v", err)
+	}
+	expectedLicenses := strings.Split(elicenses, ",")
+	alicenses, err := utils.GetMetadata(ctx, "instance", "attributes", "actual-licenses")
+	if err != nil {
+		t.Fatalf("Failed to get actual licenses: %v", err)
+	}
+	actualLicenses := strings.Split(alicenses, ",")
+
+	sort.Strings(expectedLicenseCodes)
+	sort.Strings(actualLicenseCodes)
+	if len(expectedLicenseCodes) != len(actualLicenseCodes) {
+		t.Errorf("wrong number of license codes, got %d want %d", len(actualLicenseCodes), len(expectedLicenseCodes))
+	}
+	for i := range expectedLicenseCodes {
+		if expectedLicenseCodes[i] != actualLicenseCodes[i] {
+			t.Errorf("unexpected license code at pos %d, got %s want %s", i, expectedLicenseCodes[i], actualLicenseCodes[i])
+		}
 	}
 
-	if !strings.Contains(output.Stdout, "License Status: Licensed") {
-		t.Fatalf("Activation info does not contain 'Licensed': %s", output.Stdout)
+	sort.Strings(expectedLicenses)
+	sort.Strings(actualLicenses)
+	if len(expectedLicenses) != len(actualLicenses) {
+		t.Errorf("wrong number of licenses, got %d want %d", len(actualLicenses), len(expectedLicenses))
 	}
-
+	for i := range expectedLicenses {
+		if expectedLicenses[i] != actualLicenses[i] {
+			t.Errorf("unexpected license at pos %d, got %s want %s", i, expectedLicenses[i], actualLicenses[i])
+		}
+	}
 }
