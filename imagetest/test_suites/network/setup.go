f0825eba513e6fb8d2e1ab8449ff6beeaf305baa
Amelia Crate <acrate@google.com>
2024-02-14 16:49:17 -0800

    skip alias testing on ubuntu-pro-1604

--- imagetest/test_suites/network/setup.go
+++ imagetest/test_suites/network/setup.go
@@ -64,7 +64,7 @@ func TestSetup(t *imagetest.TestWorkflow) error {
 	vm1.RunTests("TestPingVMToVM|TestDHCP|TestDefaultMTU")
 
 	multinictests := "TestStaticIP"
-	if !utils.HasFeature(t.Image, "WINDOWS") && !strings.Contains(t.Image.Name, "sles-15") && !strings.Contains(t.Image.Name, "opensuse-leap") && !strings.Contains(t.Image.Name, "ubuntu-1604") {
+	if !utils.HasFeature(t.Image, "WINDOWS") && !strings.Contains(t.Image.Name, "sles-15") && !strings.Contains(t.Image.Name, "opensuse-leap") && !strings.Contains(t.Image.Name, "ubuntu-1604") && !strings.Contains(t.Image.Name, "ubuntu-pro-1604") {
 		multinictests += "|TestAlias"
 	}
 
