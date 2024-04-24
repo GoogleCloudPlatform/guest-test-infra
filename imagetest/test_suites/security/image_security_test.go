37f244d3ed9b394596bcb8851296923489caaea2
Amelia Crate <acrate@google.com>
2024-04-24 10:28:06 -0700

    Allow Idle listeners on dynamic UDP ports on Windows

--- imagetest/test_suites/security/image_security_test.go
+++ imagetest/test_suites/security/image_security_test.go
@@ -519,7 +519,7 @@ func validateSocketsWindows(t *testing.T) {
 			t.Errorf("could not find process name listening on port %d", p)
 		}
 		pname := strings.TrimSpace(owningprocess.Stdout)
-		if p > 49152 && pname == "svchost" {
+		if p > 49152 && (pname == "svchost" || pname == "Idle") {
 			continue
 		}
 		t.Errorf("found udp listener on unexpected port %d (process name %s)", p, pname)
